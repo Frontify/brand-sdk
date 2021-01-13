9e155b1470d812870dce1c67969d19528a26353e
Samuel Alev <samuel.alev@protonmail.com>
2021-01-13 11:05:44 +0100

    feat(socket): :sparkles: Notify user of updated settings

--- src/commands/serve.ts
+++ src/commands/serve.ts
@@ -4,8 +4,10 @@ import FastifyStatic from "fastify-static";
 import FastifyWebSocket from "fastify-websocket";
 import Logger from "../utils/logger";
 import path from "path";
+import { readFileSync } from "fs";
 import { compile } from "../utils/compile";
 import { SocketMessage } from "../SocketMessage";
+import { SocketMessageType } from "../SocketMessageType";
 import { watch } from "../utils/watch";
 
 const port = Number(process.env.WS_SERVER_PORT) || 5600;
@@ -20,7 +22,7 @@ watch(
     () => {
         compile(customBlockEntryFilePath, customBlockDistPath);
     },
-    ["node_modules", ".git", "dist"],
+    ["node_modules", ".git", "dist", `${customBlockPath}/**/settings.json`],
 );
 
 const fastify: FastifyInstance = Fastify();
@@ -47,24 +49,42 @@ fastify.get("/{blockId}", (_req, res) => {
 });
 
 fastify.get("/websocket", { websocket: true }, (connection) => {
-    connection.socket.send(SocketMessage.BlockUpdated);
+    connection.socket.send(JSON.stringify({ message: SocketMessageType.BlockUpdated }));
 
-    const blocksUpdateWatcher = watch(customBlockDistPath, (event: string, path: string) => {
+    const blocksUpdateWatcher = watch(`${customBlockDistPath}/**.js`, (event: string, path: string) => {
         if (event === "change") {
             Logger.info("Notify browser of updated block", path);
-            connection.socket.send(SocketMessage.BlockUpdated);
+            connection.socket.send(JSON.stringify({ message: SocketMessageType.BlockUpdated, data: "blockname" }));
         }
     });
 
-    const settingsUpdateWatcher = watch(`${customBlockPath}/**/settings.json`, (event: string) => {
-        if (event === "change") {
-            Logger.info("Notify browser of updated settings");
-            connection.socket.send(SocketMessage.SettingsUpdated);
-        }
-    });
+    const settingsUpdateWatcher = watch(
+        `${customBlockPath}/**/settings.json`,
+        async (event: string, eventPath: string) => {
+            if (event === "change") {
+                const settingsRaw = readFileSync(eventPath, "utf8");
+                const settingsJson = JSON.parse(settingsRaw);
+                const eventPathArray = eventPath.split("/");
+                const blockName = eventPathArray[eventPathArray.length - 2];
 
-    connection.socket.on("message", (message: SocketMessage) => {
-        Logger.info(`Message received: ${message}`);
+                Logger.info("Notify browser of updated settings");
+
+                connection.socket.send(
+                    JSON.stringify({
+                        message: SocketMessageType.SettingsStructureUpdated,
+                        data: { blockName, blockSettings: settingsJson },
+                    }),
+                );
+            }
+        },
+    );
+
+    connection.socket.on("message", (socketMessage: string) => {
+        const parsedEvent: SocketMessage = JSON.parse(socketMessage);
+        const message = parsedEvent.message;
+        const data = parsedEvent.data;
+
+        Logger.info(`Message received: ${message}${data && ` with data: ${data}`}`);
 
         switch (message) {
             default: {
