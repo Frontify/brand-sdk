916fcbf047e67a50ce348bbfde3dee8da6d2ff57
Samuel Alev <samuel.alev@protonmail.com>
2021-01-14 12:08:21 +0100

    chore(deps): :fire: Remove `dotenv` deps

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,17 @@
 #!/usr/bin/env node
 
-import dotenv from "dotenv";
 import minimist from "minimist";
 import { exit } from "process";
 import { DevelopmentServer } from "./commands/serve";
 import Logger from "./utils/logger";
 import { printLogo } from "./utils/logo";
 
-dotenv.config();
-
 const parseArgs = minimist(process.argv.slice(2));
+
+printLogo();
 
 switch (parseArgs._[0]) {
     case "serve":
-        printLogo();
         Logger.info("Starting the development server...");
 
         const entryFileName = parseArgs.entry || "src/index.tsx";
