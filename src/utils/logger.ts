e1599f0369844ef73620e0a6f530b7026e930fee
Samuel Alev <samuel.alev@protonmail.com>
2021-01-14 14:56:20 +0100

    refactor(create-project): :lipstick: Add log at the end of the project creation

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -2,11 +2,19 @@ import chalk from "chalk";
 import { getCurrentTime } from "./date";
 
 export default class Logger {
+    static defaultInfo(...messages: string[]): void {
+        console.log(...messages);
+    }
+
     static info(...messages: string[]): void {
         console.log(`[${getCurrentTime()}] ${messages.join(" ")}`);
     }
 
     static error(...messages: string[]): void {
         console.error(chalk.red(`[${getCurrentTime()}] ${messages.join(" ")}`));
+    }
+
+    static spacer(width: number): string {
+        return Array(width).join(" ");
     }
 }
