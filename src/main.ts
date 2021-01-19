a88bf96f872e7ab0eae5b46352af5e432fd391b1
Samuel Alev <samuel.alev@protonmail.com>
2021-01-19 15:34:45 +0100

    refactor: Refactor to support only one block

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 import minimist from "minimist";
+import path from "path";
 import { exit } from "process";
 import { createNewProject } from "./commands/create";
 import { createDevelopmentServer } from "./commands/serve";
@@ -11,28 +12,33 @@ const parseArgs = minimist(process.argv.slice(2));
 
 printLogo();
 
-switch (parseArgs._[0]) {
-    case "serve":
-        Logger.info("Starting the development server...");
+(async () => {
+    switch (parseArgs._[0]) {
+        case "serve":
+            Logger.info("Starting the development server...");
 
-        const entryFileName = parseArgs.entry || "src/index.tsx";
-        const customBlockPath = parseArgs.dir || process.cwd();
-        const port = parseArgs.port || 5600;
+            const customBlockPath = parseArgs.dir || process.cwd();
+            const customBlockPackageJson = await import(path.join(customBlockPath, "package.json"));
 
-        createDevelopmentServer(entryFileName, customBlockPath, port);
+            const customBlockName = parseArgs.blockName || customBlockPackageJson.name || "default";
+            const entryFileName = parseArgs.entry || "src/index.tsx";
+            const port = parseArgs.port || 5600;
 
-        Logger.info(`Development server listen on port ${port}!`);
-        break;
-    case "create":
-        Logger.info("Creating a new project...");
+            createDevelopmentServer(customBlockName, entryFileName, customBlockPath, port);
 
-        const projectName = parseArgs._[1] || "";
+            Logger.info(`Development server listen on port ${port}!`);
+            break;
+        case "create":
+            Logger.info("Creating a new project...");
 
-        createNewProject(projectName);
+            const projectName = parseArgs._[1] || "";
 
-        break;
-    case "deploy":
-    default:
-        Logger.error("This command is not yet handled");
-        exit(0);
-}
+            createNewProject(projectName);
+
+            break;
+        case "deploy":
+        default:
+            Logger.error("This command is not yet handled");
+            exit(0);
+    }
+})();
