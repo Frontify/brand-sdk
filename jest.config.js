9a6a8beae17b0bbeca9b3bec0f8911dec38d4c63
Samuel Alev <samuel.alev@protonmail.com>
2021-01-22 16:02:53 +0100

    refactor(test): Rename `spec` to `test`

--- jest.config.js
+++ jest.config.js
@@ -6,6 +6,5 @@ module.exports = {
     transform: {
         "^.+\\.ts$": "ts-jest",
     },
-    testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.ts$",
-    moduleFileExtensions: ["ts", "js", "json", "node"],
+    moduleFileExtensions: ["ts", "js", "json"],
 };
