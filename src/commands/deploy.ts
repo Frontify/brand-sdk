c218cc55b538f827095ddcb9af009fcfe8efdc8d
Samuel Alev <samuel.alev@protonmail.com>
2021-06-02 11:25:11 +0200

    feat: add compilation step in deploy command

--- src/commands/deploy.ts
+++ src/commands/deploy.ts
@@ -1,60 +1,38 @@
-import { join } from "path";
 import Logger from "../utils/logger";
-import { createZip } from "../utils/zip";
-import { getFileHash } from "../utils/hash";
 import { getUser } from "../utils/user";
-import { mkdir } from "fs";
 import { getValidInstanceUrl } from "../utils/url";
+import { compile } from "../utils/compile";
+import { reactiveJson } from "../utils/reactiveJson";
+import { join } from "path";
 
-class CreateDeployment {
-    private readonly instanceUrl: string;
-    private readonly customBlockPath: string;
-    private readonly customBlockOutDir: string;
-
-    constructor(instanceUrl: string, customBlockPath = process.cwd()) {
-        this.instanceUrl = instanceUrl;
-        this.customBlockPath = customBlockPath;
-        this.customBlockOutDir = join(this.customBlockPath, ".frontify", "deploy");
-    }
-
-    async createZipBundle(): Promise<void> {
-        const ignoredDirectories = [".git/**", ".frontify/**", "node_modules/**", "dist/**"];
-
-        await new Promise((resolve, reject) => {
-            mkdir(this.customBlockOutDir, { recursive: true }, (error) => {
-                if (error) return reject(error);
-                resolve(this.customBlockOutDir);
-            });
-        });
-
-        await createZip(this.customBlockPath, join(this.customBlockOutDir, "custom_block.zip"), ignoredDirectories);
-    }
-
-    getZipBundleHash(): Promise<string> {
-        return getFileHash(join(this.customBlockOutDir, "custom_block.zip"));
-    }
-}
-
-export const createDeployment = async (instanceUrl: string, customBlockPath: string): Promise<void> => {
+export const createDeployment = async (
+    instanceUrl: string,
+    surface: string,
+    projectPath: string,
+    entryFileName: string,
+    distPath: string,
+): Promise<void> => {
     Logger.info(`Deploying the custom block...`);
 
     try {
         const cleanedInstanceUrl = getValidInstanceUrl(instanceUrl);
-        const deployment = new CreateDeployment(cleanedInstanceUrl, customBlockPath);
-
         const user = await getUser(cleanedInstanceUrl);
 
         if (user) {
             Logger.info(`You are logged in as ${user.name} (${cleanedInstanceUrl}).`);
-            Logger.info("Creating a zip bundle...");
-            await deployment.createZipBundle();
-            Logger.info("Generating a hash...");
-            const hash = await deployment.getZipBundleHash();
-            Logger.info(`Sending custom block with hash ${hash} to your Frontify instance...`);
-            //TODO: this will come in the future with the build server
-            //deployment.sendBundle(hash);
+
+            Logger.info("Getting the app id");
+            const manifest = reactiveJson<Manifest>("manifest.json");
+
+            Logger.info("Compile code");
+            await compile(projectPath, entryFileName, `${surface}_${manifest.appId}`, {
+                distPath: join(projectPath, distPath),
+                env: {
+                    NODE_ENV: "production",
+                },
+            });
         }
     } catch {
-        Logger.error("You need to give a Frontify instance URL");
+        Logger.error("The deployment has failed and was aborted.");
     }
 };
