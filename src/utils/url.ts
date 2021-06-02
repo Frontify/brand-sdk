824ef7f4b7dc3be6f352ca6c2fb4d616cf360533
Samuel Alev <samuel.alev@protonmail.com>
2021-06-02 14:32:19 +0200

    chore: add todo

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -5,6 +5,7 @@ export const getValidInstanceUrl = (url: string): string => {
     try {
         const cleanHost = url.replace(/^https?:\/\//, "");
         const parsedUrl = new URL(`https://${cleanHost}`);
+        //TODO: Maybe change it so it has http (as it will be automatically redirected)
         return parsedUrl.hostname;
     } catch {
         throw new InvalidInstanceUrlError(url);
