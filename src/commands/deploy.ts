fc3c51161f81c80a1d9e323dd87ce3c2cce55fcd
Samuel Alev <samuel.alev@protonmail.com>
2021-06-02 14:32:09 +0200

    feat: perform checks, compile code and send files as part of deploy command

--- src/commands/deploy.ts
+++ src/commands/deploy.ts
@@ -3,7 +3,22 @@ import { getUser } from "../utils/user";
 import { getValidInstanceUrl } from "../utils/url";
 import { compile } from "../utils/compile";
 import { reactiveJson } from "../utils/reactiveJson";
-import { join } from "path";
+import { join, sep } from "path";
+import fastGlob from "fast-glob";
+import { readFileAsBase64 } from "../utils/file";
+import { HttpClient } from "../utils/httpClient";
+import { promiseExec } from "../utils/promiseExec";
+import open from "open";
+
+const makeFilesDict = async (glob: string) => {
+    const folderFiles = await fastGlob(join(glob, "**"));
+    const folderFilenames = folderFiles.map((filePath) => filePath.replace(glob + sep, ""));
+
+    return folderFilenames.reduce((stack, filename, index) => {
+        stack[filename] = readFileAsBase64(folderFiles[index]);
+        return stack;
+    }, {});
+};
 
 export const createDeployment = async (
     instanceUrl: string,
@@ -21,18 +36,43 @@ export const createDeployment = async (
         if (user) {
             Logger.info(`You are logged in as ${user.name} (${cleanedInstanceUrl}).`);
 
-            Logger.info("Getting the app id");
             const manifest = reactiveJson<Manifest>("manifest.json");
 
-            Logger.info("Compile code");
+            Logger.info("Performing type checks...");
+            await promiseExec(`cd ${projectPath} && ./node_modules/.bin/tsc --noEmit`);
+
+            Logger.info("Performing eslint checks...");
+            await promiseExec(`cd ${projectPath} && ./node_modules/.bin/eslint src`);
+
+            Logger.info("Running security checks...");
+            await promiseExec(`cd ${projectPath} && npm audit --audit-level=high`);
+
+            Logger.info("Compiling code...");
             await compile(projectPath, entryFileName, `${surface}_${manifest.appId}`, {
                 distPath: join(projectPath, distPath),
                 env: {
                     NODE_ENV: "production",
                 },
             });
+
+            Logger.info("Sending the files to Frontify Marketplace...");
+
+            const request = {
+                build: await makeFilesDict(join(projectPath, distPath)),
+                source: await makeFilesDict(join(projectPath, "src")),
+            };
+
+            const httpClient = new HttpClient(instanceUrl);
+            //TODO: Add version? Do we use the OC version or semVer/incremental?
+            await httpClient.post(`/api/marketplace-app/${manifest.appId}`, request);
+
+            Logger.info("The new version have been pushed.");
+
+            Logger.info("Opening the Frontify Marketplace page");
+
+            await open(`https://${instanceUrl}/marketplace/apps/${manifest.appId}`);
         }
-    } catch {
-        Logger.error("The deployment has failed and was aborted.");
+    } catch (error) {
+        Logger.error("The deployment has failed and was aborted.", error);
     }
 };
