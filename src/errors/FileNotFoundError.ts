a4ae510e2816ada0f35c0493424601f12b91f2f8
Samuel Alev <samuel.alev@protonmail.com>
2021-06-03 09:12:19 +0200

    chore: fix typo

--- src/errors/FileNotFoundError.ts
+++ src/errors/FileNotFoundError.ts
@@ -3,6 +3,6 @@ import Logger from "../utils/logger";
 export default class FileNotFoundError extends Error {
     constructor(path: string) {
         super();
-        Logger.error(`The file at "${path}" have not been found.`);
+        Logger.error(`The file at "${path}" has not been found.`);
     }
 }
