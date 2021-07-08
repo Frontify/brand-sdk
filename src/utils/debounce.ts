6268db4eee854ab58224d6933543a922e82e1405
Samuel Alev <samuel.alev@protonmail.com>
2021-07-08 10:49:02 +0200

    chore: use lodash debounce

--- src/utils/debounce.ts
+++
@@ -1,9 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export default function debounce(callback: (...args: any) => void, delay: number): () => void {
-    let timeoutId: NodeJS.Timeout;
-    return () => {
-        clearTimeout(timeoutId);
-        // eslint-disable-next-line prefer-rest-params
-        timeoutId = setTimeout(() => callback(arguments), delay);
-    };
-}
