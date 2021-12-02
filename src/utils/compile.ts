fd150692b162c031901920c46a49f721396af2f9
Marco Bohler <triggertoo@gmail.com>
2021-12-02 17:45:20 +0100

    update

--- src/utils/compile.ts
+++ src/utils/compile.ts
@@ -1,5 +1,6 @@
 import { join } from "path";
-import { ProvidePlugin, webpack } from "webpack";
+import { resolve } from "path/posix";
+import { ProvidePlugin, NormalModuleReplacementPlugin, webpack } from "webpack";
 import InjectPlugin, { ENTRY_ORDER } from "webpack-inject-plugin";
 
 export interface CompilerOptions {
@@ -31,120 +32,101 @@ export const compile = async (
         ...options,
     };
 
-    console.log(join(projectPath, "dist"), entryFileNames);
-
-    const compiler = webpack(
-        {
-            name: iifeGlobalName,
-            context: projectPath,
-            // externals: ["react", "react-dom"],
-            entry: entryFileNames.map((entryFileName) => join(projectPath, entryFileName)),
-            output: {
-                library: iifeGlobalName,
-                libraryTarget: "umd",
-                path: join(projectPath, "dist"),
-                filename: "index.js",
-                iife: true,
-            },
-            mode: "production",
-            module: {
-                rules: [
-                    {
-                        test: /\.[jt]sx?$/,
-                        exclude: /node_modules/,
-                        use: [
-                            {
-                                loader: require.resolve("babel-loader"),
-                                options: {
-                                    presets: [
-                                        [
-                                            "@babel/preset-env",
-                                            {
-                                                modules: false,
-                                                loose: true,
-                                            },
-                                        ],
-                                        "@babel/preset-typescript",
+    const compiler = webpack({
+        context: projectPath,
+        externals: {
+            react: "React",
+            "react-dom": "ReactDOM",
+        },
+        entry: entryFileNames.map((entryFileName) => join(projectPath, entryFileName)).reverse(),
+        output: {
+            library: iifeGlobalName,
+            libraryTarget: "umd",
+            path: join(projectPath, "dist"),
+            filename: "index.js",
+            iife: true,
+        },
+        mode: "development",
+        module: {
+            rules: [
+                {
+                    test: /\.[jt]sx?$/,
+                    exclude: /node_modules/,
+                    use: [
+                        {
+                            loader: require.resolve("babel-loader"),
+                            options: {
+                                presets: [
+                                    [
+                                        "@babel/preset-env",
+                                        {
+                                            modules: false,
+                                            loose: true,
+                                        },
                                     ],
-                                    plugins: [
-                                        [
-                                            "@babel/plugin-transform-react-jsx",
-                                            {
-                                                runtime: "automatic",
-                                            },
-                                        ],
-                                        ["@babel/plugin-proposal-class-properties", { modules: false, loose: true }],
+                                    "@babel/preset-typescript",
+                                ],
+                                plugins: [
+                                    [
+                                        "@babel/plugin-transform-react-jsx",
+                                        {
+                                            runtime: "automatic",
+                                        },
                                     ],
-                                },
+                                    ["@babel/plugin-proposal-class-properties", { modules: false, loose: true }],
+                                ],
                             },
-                        ],
-                    },
-                    {
-                        test: /\.css$/,
-                        use: ["style-loader", "css-loader", "postcss-loader"],
-                    },
-                ],
-            },
-            resolve: {
-                extensions: [".js", ".ts", ".tsx", ".json"],
-            },
-            plugins: [
-                new ProvidePlugin({
-                    React: "react",
-                    "react-dom": "ReactDOM",
-                }),
-                new InjectPlugin(
-                    () => {
-                        return `console.log('this is a banner');
-            window.require = (moduleName) => {
-                switch (moduleName) {
-                    case "react":
-                        console.log('react loaded');
-                        return window["React"];
-                    case "react-dom":
-                        console.log('react-dom loaded');
-                        return window["ReactDOM"];
-                    default:
-                        throw new Error("Could not resolve module from Frontify, please install it locally: npm i", moduleName);
-                }
-            };
-            `;
-                    },
-                    { entryOrder: ENTRY_ORDER.First },
-                ),
+                        },
+                    ],
+                },
+                {
+                    test: /\.css$/,
+                    use: ["style-loader", "css-loader", "postcss-loader"],
+                },
             ],
         },
-        (err, stats) => {
-            if (err) {
-                console.error(err.stack || err);
-                if (err.name) {
-                    console.error(err.name);
-                }
-                return;
-            }
-
-            const info = stats?.toJson();
-
-            if (stats?.hasErrors()) {
-                console.error(info?.errors);
-            }
-
-            if (stats?.hasWarnings()) {
-                console.warn(info?.warnings);
-            }
+        resolve: {
+            extensions: [".js", ".ts", ".tsx", ".json"],
         },
-    );
-
-    // compiler.outputPath = join(projectPath, "dist");
-    compiler.run((error, stats) => {
-        if (error) {
-            console.log(error.message);
-        }
-        const info = stats?.toJson();
-        if (stats?.hasErrors() && info?.errors) {
-            console.log(info.errors.join(", "));
-        }
+        plugins: [
+            // new ProvidePlugin({
+            //     "window.React": "react",
+            //     "window.ReactDOM": "react-dom",
+            // }),
+            //     new InjectPlugin(
+            //         () => {
+            //             return `console.log('this is a banner');
+            // window.require = (moduleName) => {
+            //     switch (moduleName) {
+            //         case "react":
+            //             console.log('react loaded');
+            //             return window["React"];
+            //         case "react-dom":
+            //             console.log('react-dom loaded');
+            //             return window["ReactDOM"];
+            //         default:
+            //             throw new Error("Could not resolve module from Frontify, please install it locally: npm i", moduleName);
+            //     }
+            // };
+            // `;
+            //         },
+            //         { entryOrder: ENTRY_ORDER.First },
+            //     ),
+        ],
     });
+
+    return new Promise((resolve) =>
+        compiler.run((error, stats) => {
+            if (error) {
+                console.log(error.message);
+            }
+            const info = stats?.toJson();
+            if (stats?.hasErrors() && info?.errors) {
+                console.log(info.errors.join(", "));
+            }
+            resolve();
+        }),
+    );
 
     // const rollupConfig: RollupOptions = {
     //     external: ["react", "react-dom"],
