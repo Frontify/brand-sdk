98c4293ef57dd500ca184656714e314860e22484
Samuel Alev <samuel.alev@frontify.com>
2022-04-21 18:31:35 +0200

    chore: update chalk

--- rollup.config.ts
+++ rollup.config.ts
@@ -2,14 +2,20 @@ import { nodeResolve } from '@rollup/plugin-node-resolve';
 import esbuild from 'rollup-plugin-esbuild';
 import commonJs from '@rollup/plugin-commonjs';
 import json from '@rollup/plugin-json';
+import replace from '@rollup/plugin-replace';
 import pkg from './package.json';
 
-export default {
+/** @type {import('rollup').RollupOptions} */
+const rollupConfig = {
     input: 'src/index.ts',
     external: [...Object.keys(pkg.dependencies || {}), 'fsevents'],
     plugins: [
-        nodeResolve({ preferBuiltins: true }),
+        nodeResolve({ preferBuiltins: true, exportConditions: ['node'] }),
         commonJs(),
+        replace({
+            preventAssignment: true,
+            values: { 'process.env.NODE_ENV': JSON.stringify('production') },
+        }),
         json(),
         esbuild({
             minify: process.env.NODE_ENV === 'production',
@@ -18,6 +24,12 @@ export default {
             },
         }),
     ],
+    onwarn: (warning) => {
+        //TODO: Remove the silent: https://github.com/egoist/rollup-plugin-esbuild/issues/295
+        if (warning.code && 'CIRCULAR_DEPENDENCY' !== warning.code && !warning.message.includes('node:')) {
+            console.error(warning.message);
+        }
+    },
     output: [
         {
             file: 'dist/index.js',
@@ -27,3 +39,5 @@ export default {
         },
     ],
 };
+
+export default rollupConfig;
