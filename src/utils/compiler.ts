3a063d22b0cc74b3c79d6f59098c8faf7e19640d
Marco Bohler <triggertoo@gmail.com>
2022-07-26 10:59:57 +0200

    load react from external

--- src/utils/compiler.ts
+++ src/utils/compiler.ts
@@ -3,8 +3,29 @@
 import { join } from 'path';
 import { InlineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { viteExternalsPlugin } from 'vite-plugin-externals';
 
 const filesToIgnore = ['node_modules', 'package*.json', '.git', '.gitignore', 'dist'];
+
+// const reactWindowPlugin = {
+//     name: 'reactWindow',
+//     setup(build) {
+//         // Intercept import paths called "env" so esbuild doesn't attempt
+//         // to map them to a file system location. Tag them with the "env-ns"
+//         // namespace to reserve them for this plugin.
+//         build.onResolve({ filter: /^react$/ }, (args) => ({
+//             path: args.path,
+//             namespace: 'globalExternal',
+//         }));
+
+//         // Load paths tagged with the "env-ns" namespace and behave as if
+//         // they point to a JSON file containing the environment variables.
+//         build.onLoad({ filter: /.*/, namespace: 'globalExternal' }, () => ({
+//             contents: 'module.exports = globalThis.React',
+//             loader: 'js',
+//         }));
+//     },
+// };
 
 export const getViteConfig = (
     projectPath: string,
@@ -17,9 +38,15 @@ export const getViteConfig = (
         mode,
         envDir: join(__dirname, 'env'),
         root: projectPath,
-        plugins: [react()],
+        plugins: [
+            react(),
+            viteExternalsPlugin({
+                react: 'React',
+                'react-dom': 'ReactDOM',
+            }),
+        ],
 
-        // logLevel: 'warn',
+        logLevel: 'info',
         build: {
             // polyfillModulePreload: false,
             lib: {
