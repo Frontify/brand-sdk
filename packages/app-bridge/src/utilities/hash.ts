da09acd5d13bcf6822e999b471bd6dee85f17b3a
Samuel Alev <samuel.alev@frontify.com>
2022-12-01 09:48:51 +0100

    chore: revert

--- packages/app-bridge/src/utilities/hash.ts
+++ packages/app-bridge/src/utilities/hash.ts
@@ -1,8 +1,5 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
 export const generateRandomString = (length = 6): string => {
-    const typedArray = new Uint8Array(1);
-    const randomValue = window.crypto.getRandomValues(typedArray)[0];
-    const randomFloat = randomValue / Math.pow(2, 8);
-    return randomFloat.toString(20).substring(2, length);
+    return Math.random().toString(20).substr(2, length);
 };
