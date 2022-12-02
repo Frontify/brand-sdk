f14051013520f73a9e60bcbac704eb607a69468a
Samuel Alev <samuel.alev@frontify.com>
2022-12-02 11:25:07 +0100

    feat: add doc in type and custom section handling

--- packages/guideline-blocks-settings/src/index.ts
+++ packages/guideline-blocks-settings/src/index.ts
@@ -1,11 +1,11 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
+import type { FC } from 'react';
 import type { AppBridgeBlock } from '@frontify/app-bridge';
 import type {
     Bundle as BundleSidebarSettings,
     SettingBlock as SettingBlockSidebarSettings,
 } from '@frontify/sidebar-settings';
-import type { FC } from 'react';
 
 export * from '@frontify/sidebar-settings';
 
@@ -27,18 +27,40 @@ export type BlockSettingsStructureExport = {
     [Sections.Layout]?: SettingBlock[];
     [Sections.Style]?: SettingBlock[];
     [Sections.Security]?: SettingBlock[];
-};
+} & { [customSectionName: string]: SettingBlock[] };
 
 export type BlockProps = {
+    /**
+     * The Frontify App Bridge provides an interface to the Frontify app internals.
+     * {@link https://developer.frontify.com/document/1366#/details-concepts/content-blocks/introducing-the-appbridge}
+     */
     appBridge: AppBridgeBlock;
 };
 
 export type BlockConfigExport = {
+    /**
+     * Block component to render.
+     * {@link https://developer.frontify.com/document/1366#/details-concepts/content-blocks}
+     */
     block: FC<BlockProps>;
+    /**
+     * Contains the block settings and its structure.
+     * {@link https://developer.frontify.com/document/1366#/details-concepts/block-settings}
+     */
     settings: ReturnType<typeof defineSettings>;
+    /**
+     * Block lifecycle hook ran before the block gets added in the Guideline.
+     * The hook support both synchronous or asynchronous execution.
+     * {@link https://developer.frontify.com/document/1366#/details-concepts/new-block-lifecycle/on-block-creation}
+     */
     onBlockCreated?:
         | (({ appBridge }: { appBridge: AppBridgeBlock }) => void)
         | (({ appBridge }: { appBridge: AppBridgeBlock }) => Promise<void>);
+    /**
+     * Block lifecycle hook ran before the block gets deleted from the Guideline.
+     * The hook support both synchronous or asynchronous execution.
+     * {@link https://developer.frontify.com/document/1366#/details-concepts/new-block-lifecycle/on-block-deletion}
+     */
     onBlockDeleted?:
         | (({ appBridge }: { appBridge: AppBridgeBlock }) => void)
         | (({ appBridge }: { appBridge: AppBridgeBlock }) => Promise<void>);
