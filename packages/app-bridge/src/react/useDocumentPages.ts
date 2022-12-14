9b1ad6db4f112e1ef05e01d4f1857e2a2ce94892
Goran Ribic <goran.ribic@frontify.com>
2022-12-14 12:20:37 +0100

    fix: missing update of page in category in event

--- packages/app-bridge/src/react/useDocumentPages.ts
+++ packages/app-bridge/src/react/useDocumentPages.ts
@@ -33,29 +33,22 @@ export const useDocumentPages = (appBridge: AppBridgeTheme, documentId: number) 
             setDocumentPages((previousState) => {
                 const isInitial = previousState === null;
 
+                const item = event.documentPageOrDocumentCategory as DocumentPage | DocumentCategory;
+
                 if (isInitial) {
-                    return event.action === 'update' || event.action === 'add'
-                        ? [event.documentPageOrDocumentCategory as DocumentPage | DocumentCategory]
-                        : previousState;
+                    return event.action === 'update' || event.action === 'add' ? [item] : previousState;
                 }
 
                 if (event.action === 'delete') {
-                    return deletePage(previousState, event.documentPageOrDocumentCategory);
+                    return deleteItem(previousState, event.documentPageOrDocumentCategory);
                 }
 
                 if (event.action === 'add') {
-                    return addPage(
-                        previousState,
-                        event.documentPageOrDocumentCategory as DocumentPage | DocumentCategory,
-                        documentId,
-                    );
+                    return addItem(previousState, item, documentId);
                 }
 
                 if (event.action === 'update') {
-                    return updatePage(
-                        previousState,
-                        event.documentPageOrDocumentCategory as DocumentPage | DocumentCategory,
-                    );
+                    return updateItem(previousState, item);
                 }
 
                 return previousState;
@@ -90,62 +83,92 @@ export const useDocumentPages = (appBridge: AppBridgeTheme, documentId: number) 
     return [documentPages];
 };
 
-const addPage = (
-    pages: (DocumentPage | DocumentCategory)[],
-    pageToAdd: DocumentPage | DocumentCategory,
+const addItem = (
+    items: (DocumentPage | DocumentCategory)[],
+    itemToAdd: DocumentPage | DocumentCategory,
     currentDocument: number,
 ) => {
-    if (pageToAdd.documentId !== currentDocument) {
-        return pages;
+    if (itemToAdd.documentId !== currentDocument) {
+        return items;
     }
 
-    const pagesClone = cloneDeep(pages);
+    const itemsClone = cloneDeep(items);
 
     const isPageInCategory =
-        'categoryId' in pageToAdd && pageToAdd.categoryId !== null && pageToAdd.categoryId !== undefined;
+        'categoryId' in itemToAdd && itemToAdd.categoryId !== null && itemToAdd.categoryId !== undefined;
 
     const addPageToCategory = () => {
-        for (const page of pagesClone) {
-            if (
-                page.id === (pageToAdd as DocumentPage).categoryId &&
-                page.documentId === pageToAdd.documentId &&
-                'documentPages' in page
-            ) {
-                page.documentPages
-                    ? page.documentPages.push(pageToAdd as DocumentPage)
-                    : (page.documentPages = [pageToAdd as DocumentPage]);
+        for (const item of itemsClone) {
+            const isCategory = 'documentPages' in item;
+            const itShouldAdd =
+                item.id === (itemToAdd as DocumentPage).categoryId && item.documentId === itemToAdd.documentId;
+
+            if (isCategory && itShouldAdd) {
+                if (item.documentPages) {
+                    item.documentPages.push(itemToAdd as DocumentPage);
+                } else {
+                    item.documentPages = [itemToAdd as DocumentPage];
+                }
 
                 break;
             }
         }
     };
 
-    isPageInCategory ? addPageToCategory() : pagesClone.push(pageToAdd);
+    isPageInCategory ? addPageToCategory() : itemsClone.push(itemToAdd);
 
-    return pagesClone;
+    return itemsClone;
 };
 
-const updatePage = (
-    documents: (DocumentPage | DocumentCategory)[],
-    documentToUpdate: DocumentPage | DocumentCategory,
-) => {
-    const pagesClone = cloneDeep(documents);
+const updateItem = (items: (DocumentPage | DocumentCategory)[], itemToUpdate: DocumentPage | DocumentCategory) => {
+    const itemsClone = cloneDeep(items);
 
-    const documentToUpdateIndex = pagesClone.findIndex((document) => document.id === documentToUpdate.id);
+    const isPageInCategory =
+        'categoryId' in itemToUpdate && itemToUpdate.categoryId !== null && itemToUpdate.categoryId !== undefined;
 
-    pagesClone[documentToUpdateIndex] = documentToUpdate;
+    const updatePageInCategory = () => {
+        for (const page of itemsClone) {
+            const isCategory = 'documentPages' in page;
 
-    return pagesClone;
+            const itShouldUpdate =
+                page.id === (itemToUpdate as DocumentPage).categoryId && page.documentId === itemToUpdate.documentId;
+
+            if (isCategory && itShouldUpdate) {
+                const pageToUpdateIndex = page.documentPages.findIndex((page) => page.id === itemToUpdate.id);
+
+                page.documentPages[pageToUpdateIndex] = {
+                    ...page.documentPages[pageToUpdateIndex],
+                    ...(itemToUpdate as DocumentPage),
+                };
+
+                break;
+            }
+        }
+    };
+
+    if (isPageInCategory) {
+        updatePageInCategory();
+
+        return itemsClone;
+    }
+
+    const pageToUpdateIndex = itemsClone.findIndex((document) => document.id === itemToUpdate.id);
+
+    if (pageToUpdateIndex !== -1) {
+        itemsClone[pageToUpdateIndex] = itemToUpdate;
+    }
+
+    return itemsClone;
 };
 
-const deletePage = (pages: (DocumentPage | DocumentCategory)[], pageToDelete: { id: number }) => {
-    const filteredPages = pages.filter((page) => page.id !== pageToDelete.id);
+const deleteItem = (items: (DocumentPage | DocumentCategory)[], itemToDelete: { id: number }) => {
+    const filteredItems = items.filter((item) => item.id !== itemToDelete.id);
 
-    return filteredPages.map((page) => {
-        if ('documentPages' in page) {
-            page.documentPages = page.documentPages.filter((page) => page.id !== pageToDelete.id);
+    return filteredItems.map((item) => {
+        if ('documentPages' in item) {
+            item.documentPages = item.documentPages.filter((page) => page.id !== itemToDelete.id);
         }
 
-        return page;
+        return item;
     });
 };
