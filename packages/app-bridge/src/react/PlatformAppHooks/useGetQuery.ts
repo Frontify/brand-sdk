c36b50a9e4e2d83e6ab5e66012e03bd169b6d430
julianiff <julian.iff@frontify.com>
2023-02-01 13:53:42 +0100

    Remove void response type

--- packages/app-bridge/src/react/PlatformAppHooks/useGetQuery.ts
+++ packages/app-bridge/src/react/PlatformAppHooks/useGetQuery.ts
@@ -8,18 +8,17 @@ import { PlatformAppContext } from '../PlatformApp';
  * Returns a function to fetch
  * Data with handled Auth
  */
-export const useGetQuery = (): ((query: string) => Promise<unknown>) | void => {
+export const useGetQuery = (): ((query: string) => Promise<unknown>) => {
     // Here we would need to be sure that the token is available and valid
     const { token } = useContext<PlatformAppProperties>(PlatformAppContext);
 
     if (!token) {
         console.warn('No Authentication token available!');
-        return;
     }
 
     return async (query: string) => {
         try {
-            const res = await fetch(getGraphQlEndpoint(token?.bearerToken.domain), {
+            const res = await fetch(getGraphQlEndpoint(token!.bearerToken.domain), {
                 method: 'POST',
                 headers: {
                     Accept: 'application/json',
