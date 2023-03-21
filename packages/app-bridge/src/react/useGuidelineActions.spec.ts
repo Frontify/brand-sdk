0933c7d14b6c5350c9f0855110461cdced9ec42c
leko91 <nikola.milenkovic@frontify.com>
2023-03-21 17:26:50 +0100

    fix: adjust test

--- packages/app-bridge/src/react/useGuidelineActions.spec.ts
+++ packages/app-bridge/src/react/useGuidelineActions.spec.ts
@@ -669,10 +669,13 @@ describe('useGuidelineActions hook', () => {
 
         await waitFor(() => {
             expect(updatePage).toHaveBeenCalledWith(page);
-            expect(emitSpy).toHaveBeenCalledWith(`AppBridge:GuidelineDocumentPageAction:${page.documentId}`, {
-                documentPage: DocumentPageDummy.with(1),
-                action: 'update',
-            });
+            expect(emitSpy).toHaveBeenCalledWith(
+                `AppBridge:GuidelineDocumentPageAction:${DocumentPageDummy.with(1).documentId}`,
+                {
+                    documentPage: DocumentPageDummy.with(1),
+                    action: 'update',
+                },
+            );
         });
     });
 
