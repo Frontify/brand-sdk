b4f02fcc8fead15000b6eefb65fb598df24af74b
Rafael Giezendanner <rafael.giezendanner@frontify.com>
2023-03-29 11:06:39 +0200

    feat: adding useBulkDownload hook (#381)
    Co-authored-by: Samuel Alev <samuel.alev@frontify.com>

---
+++ packages/app-bridge/src/react/useBulkDownload.ts
@@ -0,0 +1,69 @@
+/* (c) Copyright Frontify Ltd., all rights reserved. */
+
+import { useRef, useState } from 'react';
+import type { AppBridgeBlock } from '../AppBridgeBlock';
+
+export enum BulkDownloadState {
+    Init = 'init',
+    Started = 'started',
+    Pending = 'pending',
+    Ready = 'ready',
+    Error = 'error',
+}
+
+export const useBulkDownload = (appBridge: AppBridgeBlock) => {
+    const intervalId = useRef<number | null>(null);
+    const [status, setStatus] = useState<BulkDownloadState>(BulkDownloadState.Init);
+    const [downloadUrl, setDownloadUrl] = useState<Nullable<string>>(null);
+
+    const generateBulkDownload = async (assetIds: number[], setIds?: number[]) => {
+        try {
+            setStatus(BulkDownloadState.Started);
+
+            const token = await appBridge.getBulkDownloadToken(assetIds, setIds);
+
+            setDownloadUrl(null);
+            startDownload(token);
+        } catch (error) {
+            setStatus(BulkDownloadState.Error);
+            console.error(error);
+        }
+    };
+
+    const startDownload = async (token: string) => {
+        try {
+            const download = await appBridge.getBulkDownloadByToken(token);
+
+            if (download.downloadUrl) {
+                setDownloadUrl(download.downloadUrl);
+                setStatus(BulkDownloadState.Ready);
+            } else {
+                setStatus(BulkDownloadState.Pending);
+                intervalId.current = listenForBulkDownloadReady(download.signature);
+            }
+        } catch (error) {
+            setStatus(BulkDownloadState.Error);
+            console.error(error);
+        }
+    };
+
+    const listenForBulkDownloadReady = (signature: string) => {
+        return window.setInterval(async () => {
+            try {
+                const download = await appBridge.getBulkDownloadBySignature(signature);
+
+                if (download.downloadUrl) {
+                    setStatus(BulkDownloadState.Ready);
+                    setDownloadUrl(download.downloadUrl);
+                    intervalId.current && clearInterval(intervalId.current);
+                }
+            } catch (error) {
+                setStatus(BulkDownloadState.Error);
+                console.error(error);
+                intervalId.current && clearInterval(intervalId.current);
+            }
+        }, 2500);
+    };
+
+    return { generateBulkDownload, status, downloadUrl };
+};
