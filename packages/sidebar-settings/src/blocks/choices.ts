510948d71d3df9139802d7962de97b4ea2abdcc8
Samuel Alev <samuel.alev@frontify.com>
2023-04-12 19:28:03 +0200

    feat: add custom icon support in segmented controls/dropdown/input

--- packages/sidebar-settings/src/blocks/choices.ts
+++ packages/sidebar-settings/src/blocks/choices.ts
@@ -1,5 +1,7 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
+import { ReactElement } from 'react';
+
 import type { IconEnum } from '.';
 import type { BaseBlock, ValueOrPromisedValue } from './base';
 
@@ -14,7 +16,7 @@ export type Choice = {
      *
      * The full list of icons can be found here {@link https://github.com/Frontify/fondue/blob/beta/src/foundation/Icon/IconEnum.ts}
      */
-    icon?: IconEnum | keyof typeof IconEnum;
+    icon?: IconEnum | keyof typeof IconEnum | ReactElement;
 
     /**
      * The value of the item.
--- packages/sidebar-settings/src/blocks/input.ts
+++ packages/sidebar-settings/src/blocks/input.ts
@@ -1,5 +1,7 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
+import { ReactElement } from 'react';
+
 import type { Rule } from '../helpers';
 import type { BaseBlock } from './base';
 import type { IconEnum, TextInputType } from '.';
@@ -15,7 +17,7 @@ export type InputBlock<AppBridge> = {
      *
      * The full list of icons can be found here {@link https://github.com/Frontify/fondue/blob/beta/src/foundation/Icon/IconEnum.ts}
      */
-    icon?: IconEnum | keyof typeof IconEnum;
+    icon?: IconEnum | keyof typeof IconEnum | ReactElement;
 
     /**
      * The type of input (text, number, etc.).
