0d76474cd6e806c2b44b666da461e996b3c86670
Samuel Alev <samuel.alev@frontify.com>
2023-05-15 18:21:18 +0200

    lint fix

--- packages/app-bridge/src/react/useDocuments.spec.ts
+++ packages/app-bridge/src/react/useDocuments.spec.ts
@@ -2,39 +2,66 @@
 
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
-import { afterEach, describe, expect, it, vi } from 'vitest';
-import { renderHook, waitFor } from '@testing-library/react';
+import { afterEach, describe, it, vi } from 'vitest';
 
 describe('useDocument', () => {
     afterEach(() => {
         vi.resetAllMocks();
     });
 
-    it('should fetch documents on mount', async () => {});
+    it('should fetch documents on mount', async () => {
+        // TODO: Implement
+    });
 
-    it('should not fetch documents on mount if not enabled', () => {});
+    it('should not fetch documents on mount if not enabled', () => {
+        // TODO: Implement
+    });
 
-    it('should fetch documents if it gets enabled', async () => {});
+    it('should fetch documents if it gets enabled', async () => {
+        // TODO: Implement
+    });
 
-    it('should update documents if a document is added', async () => {});
+    it('should update documents if a document is added', async () => {
+        // TODO: Implement
+    });
 
-    it('should update documents if a document is removed', async () => {});
+    it('should update documents if a document is removed', async () => {
+        // TODO: Implement
+    });
 
-    it('should update documents if a document is updated', async () => {});
+    it('should update documents if a document is updated', async () => {
+        // TODO: Implement
+    });
 
-    it('should return ungrouped documents and update them after changes', async () => {});
+    it('should return ungrouped documents and update them after changes', async () => {
+        // TODO: Implement
+    });
 
-    it('should return grouped documents and update them after changes', async () => {});
+    it('should return grouped documents and update them after changes', async () => {
+        // TODO: Implement
+    });
 
-    it('should update number of categorized page if an uncategorized document page is added', async () => {});
+    it('should update number of categorized page if an uncategorized document page is added', async () => {
+        // TODO: Implement
+    });
 
-    it('should not update number of categorized page if a categorized document page is added', async () => {});
+    it('should not update number of categorized page if a categorized document page is added', async () => {
+        // TODO: Implement
+    });
 
-    it('should update number of categorized page if an uncategorized document page is removed', async () => {});
+    it('should update number of categorized page if an uncategorized document page is removed', async () => {
+        // TODO: Implement
+    });
 
-    it('should not update number of categorized page if a categorized document page is removed', async () => {});
+    it('should not update number of categorized page if a categorized document page is removed', async () => {
+        // TODO: Implement
+    });
 
-    it('should update number of document category if added', async () => {});
+    it('should update number of document category if added', async () => {
+        // TODO: Implement
+    });
 
-    it('should update number of document category if remove', async () => {});
+    it('should update number of document category if remove', async () => {
+        // TODO: Implement
+    });
 });
