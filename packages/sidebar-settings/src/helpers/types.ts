b4a8e84e8ce6bd597784d08450005faf9c8f06ea
Leo Zurbriggen <2656903+lzurbriggen@users.noreply.github.com>
2023-06-22 14:52:40 +0200

    feat: add duration units to Unit type (#444)

--- packages/sidebar-settings/src/helpers/types.ts
+++ packages/sidebar-settings/src/helpers/types.ts
@@ -1,5 +1,5 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
-export type Unit = 'px' | 'em' | 'rem' | '%';
+export type Unit = 'px' | 'em' | 'rem' | '%' | 's' | 'ms';
 
 export type MultiInputTuple = [string, string, string, string];
