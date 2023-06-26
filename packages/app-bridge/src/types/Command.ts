80ff7e5da1a84438e192e08ab5079174cfef3906
Utkarsh Mishra <utkarsh.mishra@frontify.com>
2023-06-26 16:36:15 +0200

    Added declaration for off methods

--- packages/app-bridge/src/types/Command.ts
+++ packages/app-bridge/src/types/Command.ts
@@ -6,9 +6,13 @@ export type CommandResponse = {
     AssetViewer: {
         on(event: 'assetViewerOpened', callback: () => void): void;
         on(event: 'assetViewerClosed', callback: () => void): void;
+        off(event: 'assetViewerOpened'): void;
+        off(event: 'assetViewerClosed'): void;
     };
     AssetChooser: {
         on(event: 'assetChooserOpened', callback: (selectedAssets: Asset[]) => void): void;
         on(event: 'assetChooserClosed', callback: () => void): void;
+        off(event: 'assetChooserOpened'): void;
+        off(event: 'assetChooserClosed'): void;
     };
 };
