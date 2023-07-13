ebc06d030cc83482a5898b9d505e86f862e0f5d6
triggertoo <triggertoo@gmail.com>
2023-07-13 11:06:34 +0200

    feat: add Tracking.Send command

--- packages/app-bridge/src/types/DispatchHandler.ts
+++ packages/app-bridge/src/types/DispatchHandler.ts
@@ -1,10 +1,15 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
+import { Asset } from './Asset';
 import { AssetChooserOptions } from './Terrific';
 
 export type DispatchOption = {
     'AssetChooser.Open': AssetChooserOptions;
     'AssetChooser.Close': void;
+    'Tracking.Send': {
+        name: 'download asset';
+        data: Asset;
+    };
 };
 
 export type DispatchHandler<CommandName extends keyof DispatchOption> = {
