fb4e464425a3f911d00a824e0b12dbba496c1831
triggertoo <triggertoo@gmail.com>
2023-07-13 14:31:15 +0200

    update

--- packages/app-bridge/src/types/DispatchHandler.ts
+++ packages/app-bridge/src/types/DispatchHandler.ts
@@ -10,8 +10,6 @@ export type DispatchOption = {
         name: 'download asset';
         data: {
             asset: Asset;
-            downloadType: string;
-            via: string;
         };
     };
 };
