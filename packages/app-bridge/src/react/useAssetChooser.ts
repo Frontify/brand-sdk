2021328c4a5fdef3be385596e35b69dad8999cff
Samuel Alev <samuel.alev@frontify.com>
2023-07-14 09:11:32 +0200

    revert: changes in hook

--- packages/app-bridge/src/react/useAssetChooser.ts
+++ packages/app-bridge/src/react/useAssetChooser.ts
@@ -2,7 +2,6 @@
 
 import type { AppBridgeBlock } from '../AppBridgeBlock';
 import type { Asset, AssetChooserOptions } from '../types';
-import { closeAssetChooser, openAssetChooser } from '../commands/AssetChooser';
 
 type UseAssetChooserType = {
     openAssetChooser: (callback: (selectedAsset: Asset[]) => void, options: AssetChooserOptions) => void;
@@ -11,12 +10,7 @@ type UseAssetChooserType = {
 
 export const useAssetChooser = (appBridge: AppBridgeBlock): UseAssetChooserType => {
     return {
-        openAssetChooser: (callback: (selectedAsset: Asset[]) => void, options: AssetChooserOptions) => {
-            appBridge.dispatch(openAssetChooser(options));
-            appBridge.subscribe('assetsChosen', callback);
-        },
-        closeAssetChooser: () => {
-            appBridge.dispatch(closeAssetChooser());
-        },
+        openAssetChooser: appBridge.openAssetChooser.bind(appBridge),
+        closeAssetChooser: appBridge.closeAssetChooser.bind(appBridge),
     };
 };
