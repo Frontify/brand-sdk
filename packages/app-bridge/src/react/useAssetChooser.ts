c667ee2c505cf941d1c0618a9cf3fea0117a627a
Utkarsh Mishra <utkarsh.mishra@frontify.com>
2023-07-14 10:40:15 +0200

    Revert "Moving hook updates to a separate PR"
    This reverts commit 3c82feed582e88a5d6a9377f38707ae3582bdc11.

--- packages/app-bridge/src/react/useAssetChooser.ts
+++ packages/app-bridge/src/react/useAssetChooser.ts
@@ -1,16 +1,22 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
 import type { AppBridgeBlock } from '../AppBridgeBlock';
-import type { Asset, AssetChooserOptions } from '../types';
+import { closeAssetChooser, openAssetChooser } from '../commands/AssetChooser';
+import { type Asset, type AssetChooserOptions } from '../types';
 
-type UseAssetChooserType = {
+export type UseAssetChooserType = {
     openAssetChooser: (callback: (selectedAsset: Asset[]) => void, options: AssetChooserOptions) => void;
     closeAssetChooser: () => void;
 };
 
 export const useAssetChooser = (appBridge: AppBridgeBlock): UseAssetChooserType => {
     return {
-        openAssetChooser: appBridge.openAssetChooser.bind(appBridge),
-        closeAssetChooser: appBridge.closeAssetChooser.bind(appBridge),
+        openAssetChooser: (callback: (selectedAsset: Asset[]) => void, options: AssetChooserOptions) => {
+            appBridge.dispatch(openAssetChooser(options));
+            appBridge.subscribe('assetsChosen', callback);
+        },
+        closeAssetChooser: () => {
+            appBridge.dispatch(closeAssetChooser());
+        },
     };
 };
