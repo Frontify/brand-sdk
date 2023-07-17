05508c2b99d09c2e2b40290b0f46ad16d95f0b63
Utkarsh Mishra <utkarsh.mishra@frontify.com>
2023-07-17 10:56:13 +0200

    feat: Dispatch Commands and Callback subscriber for Asset Viewer and Template Chooser (#477)
    Co-authored-by: triggertoo <triggertoo@gmail.com>
    Co-authored-by: silviojaeger <silvio.jaeger@hotmail.com>
    Co-authored-by: Samuel Alev <samuel.alev@frontify.com>

--- packages/app-bridge/src/types/SubscriptionHandler.ts
+++ packages/app-bridge/src/types/SubscriptionHandler.ts
@@ -1,9 +1,11 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
 import type { Asset } from './Asset';
+import type { TemplateLegacy } from './TemplateLegacy';
 
 export type Subscription = keyof SubscriptionCallback;
 
 export type SubscriptionCallback = {
     assetsChosen: (selectedAssets: Asset[]) => void;
+    templateChosen: (selectedTemplate: TemplateLegacy) => void;
 };
