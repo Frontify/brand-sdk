4ff3b17663792c9ac2e16db9c05d7703dc817860
Utkarsh Mishra <utkarsh.mishra@frontify.com>
2023-07-17 11:04:50 +0200

    feat: dispatch/subscribe for Navigation manager (#471)

---
+++ packages/app-bridge/src/commands/NavigationManager.ts
@@ -0,0 +1,7 @@
+/* (c) Copyright Frontify Ltd., all rights reserved. */
+
+import type { DispatchHandler } from '../types';
+
+export const openNavigationManager = (): DispatchHandler<'openNavigationManager'> => ({
+    name: 'openNavigationManager',
+});
