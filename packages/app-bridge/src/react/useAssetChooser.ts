f7c8b15f52f868b369cbeb9c2d38d10bf060dd4f
Samuel Alev <samuel.alev@frontify.com>
2023-07-17 11:08:50 +0200

    revert: `useAssetChooser` changes

--- packages/app-bridge/src/react/useAssetChooser.ts
+++ packages/app-bridge/src/react/useAssetChooser.ts
@@ -1,22 +1,16 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
 import type { AppBridgeBlock } from '../AppBridgeBlock';
-import { closeAssetChooser, openAssetChooser } from '../commands/AssetChooser';
-import { type Asset, type AssetChooserOptions } from '../types';
+import type { Asset, AssetChooserOptions } from '../types';
 
-export type UseAssetChooserType = {
+type UseAssetChooserType = {
     openAssetChooser: (callback: (selectedAsset: Asset[]) => void, options: AssetChooserOptions) => void;
     closeAssetChooser: () => void;
 };
 
 export const useAssetChooser = (appBridge: AppBridgeBlock): UseAssetChooserType => {
     return {
-        openAssetChooser: (callback: (selectedAsset: Asset[]) => void, options: AssetChooserOptions) => {
-            appBridge.dispatch(openAssetChooser(options));
-            appBridge.subscribe('assetsChosen', callback);
-        },
-        closeAssetChooser: () => {
-            appBridge.dispatch(closeAssetChooser());
-        },
+        openAssetChooser: appBridge.openAssetChooser.bind(appBridge),
+        closeAssetChooser: appBridge.closeAssetChooser.bind(appBridge),
     };
 };
