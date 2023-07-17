5c5b0730cbf37493d10f9762fda1148053f7ffda
julianiff <julian.iff@frontify.com>
2023-07-17 08:14:45 +0200

    chore: rename

--- packages/app-bridge/src/types/PlatformAppMethods.ts
+++ packages/app-bridge/src/types/PlatformAppMethods.ts
@@ -1,5 +1,5 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
-export type PlatformAppPublicMethods = {
+export type PlatformAppMethods = {
     currentUser: [{ name: 'currentUser' }, { id: string; name?: string | null; email: string; avatar: string }];
 };
