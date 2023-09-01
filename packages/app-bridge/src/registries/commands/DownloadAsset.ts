ee0903235e29526b5744e2afc226094b2e451cc7
Utkarsh Mishra <utkarsh.mishra@frontify.com>
2023-09-01 16:10:58 +0200

    Add command param handler

---
+++ packages/app-bridge/src/registries/commands/DownloadAsset.ts
@@ -0,0 +1,11 @@
+/* (c) Copyright Frontify Ltd., all rights reserved. */
+
+import { DispatchHandlerParameter } from '../../AppBridge';
+import { CommandRegistry } from './CommandRegistry';
+
+export const downloadAsset = ({
+    asset,
+}: CommandRegistry['downloadAsset']): DispatchHandlerParameter<'downloadAsset', CommandRegistry> => ({
+    name: 'downloadAsset',
+    payload: { asset },
+});
