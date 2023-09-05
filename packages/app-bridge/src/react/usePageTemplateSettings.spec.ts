a1354ff7333be2c97600a40d9ec6691a546d886e
Fabricio Mendonca <fabricio.mendonca@frontify.com>
2023-09-05 16:24:02 +0200

    feat(Themes): merge theme and page template settings

--- packages/app-bridge/src/react/usePageTemplateSettings.spec.ts
+++ packages/app-bridge/src/react/usePageTemplateSettings.spec.ts
@@ -1,19 +1,41 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
 import { cleanup, renderHook, waitFor } from '@testing-library/react';
-import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest';
+import { Mock, afterEach, beforeEach, describe, expect, it, vi } from 'vitest';
 import { getAppBridgeThemeStub } from '../tests';
 import { usePageTemplateSettings } from './usePageTemplateSettings';
+import { useThemeSettings } from '.';
+// import { useThemeSettings } from './useThemeSettings';
 
 const DOCUMENT_ID = 3462;
+const THEME_SETTINGS = {
+    customThemeSetting: 123,
+    themeExclusiveSetting: 234,
+};
 const PAGE_SETTINGS = {
     myCustomSetting: 123,
 };
 
+const PAGE_SETTINGS_WITH_OVERRIDES = {
+    ...PAGE_SETTINGS,
+    customThemeSetting: 321,
+};
+
+const PAGE_SETTINGS_WITH_NULL_OVERRIDES = {
+    ...PAGE_SETTINGS,
+    customThemeSetting: null,
+};
+
+vi.mock('./useThemeSettings', () => {
+    return {
+        useThemeSettings: vi.fn(),
+    };
+});
+
 describe('usePageTemplateSettings', () => {
     beforeEach(() => {
-        // eslint-disable-next-line @typescript-eslint/no-empty-function
-        vi.spyOn(console, 'error').mockImplementation(() => {});
+        vi.spyOn(console, 'error').mockImplementation(() => void 0);
+        (useThemeSettings as Mock).mockImplementation(() => ({ themeSettings: {} }));
     });
 
     afterEach(() => {
@@ -62,6 +84,9 @@ describe('usePageTemplateSettings', () => {
             expect(result.current.isLoading).toEqual(false);
             expect(result.current.pageTemplateSettings).toEqual({ ...PAGE_SETTINGS, myCustomSetting: 456 });
         });
+
+        // Reset the object to avoid mutation in other tests
+        await result.current.updatePageTemplateSettings({ myCustomSetting: 123 });
     });
 
     it('returns the page settings for document page', async () => {
@@ -110,5 +135,93 @@ describe('usePageTemplateSettings', () => {
         });
 
         expect(console.error).toHaveBeenCalledOnce();
+    });
+
+    describe('Theme and Page template overrides', () => {
+        beforeEach(() => {
+            (useThemeSettings as Mock).mockImplementation(() => ({ themeSettings: THEME_SETTINGS }));
+        });
+        it('returns the page template settings merged with theme settings and the list of custom overrides', async () => {
+            const { result } = await loadUsePageTemplateSettings(PAGE_SETTINGS_WITH_OVERRIDES, 'cover');
+
+            expect(result.current.isLoading).toEqual(true);
+
+            await waitFor(() => {
+                expect(result.current.isLoading).toEqual(false);
+                expect(result.current.pageTemplateSettings).toEqual({
+                    ...THEME_SETTINGS,
+                    ...PAGE_SETTINGS_WITH_OVERRIDES,
+                });
+                expect(result.current.customOverrides).toEqual(['customThemeSetting']);
+            });
+        });
+
+        it('returns the page template settings merged with theme settings and no overrides', async () => {
+            const { result } = await loadUsePageTemplateSettings(PAGE_SETTINGS, 'cover');
+
+            expect(result.current.isLoading).toEqual(true);
+
+            await waitFor(() => {
+                expect(result.current.isLoading).toEqual(false);
+                expect(result.current.pageTemplateSettings).toEqual({
+                    ...PAGE_SETTINGS,
+                    ...THEME_SETTINGS,
+                });
+                expect(result.current.customOverrides).toEqual([]);
+            });
+        });
+
+        it('returns an empty object if no theme settins and no page template settings', async () => {
+            (useThemeSettings as Mock).mockImplementation(() => ({ themeSettings: {} }));
+            const { result } = await loadUsePageTemplateSettings({}, 'cover');
+
+            expect(result.current.isLoading).toEqual(true);
+
+            await waitFor(() => {
+                expect(result.current.isLoading).toEqual(false);
+                expect(result.current.pageTemplateSettings).toEqual({});
+                expect(result.current.customOverrides).toEqual([]);
+            });
+        });
+
+        it('returns only the theme settings if no page template settings', async () => {
+            const { result } = await loadUsePageTemplateSettings({}, 'cover');
+
+            expect(result.current.isLoading).toEqual(true);
+
+            await waitFor(() => {
+                expect(result.current.isLoading).toEqual(false);
+                expect(result.current.pageTemplateSettings).toEqual(THEME_SETTINGS);
+                expect(result.current.customOverrides).toEqual([]);
+            });
+        });
+
+        it('returns only the page template settings if no theme settings', async () => {
+            (useThemeSettings as Mock).mockImplementation(() => ({ themeSettings: {} }));
+            const { result } = await loadUsePageTemplateSettings(PAGE_SETTINGS, 'cover');
+
+            expect(result.current.isLoading).toEqual(true);
+
+            await waitFor(() => {
+                expect(result.current.isLoading).toEqual(false);
+                expect(result.current.pageTemplateSettings).toEqual(PAGE_SETTINGS);
+                expect(result.current.customOverrides).toEqual([]);
+            });
+        });
+
+        it('returns theme setting value if page template override is null', async () => {
+            const { result } = await loadUsePageTemplateSettings(PAGE_SETTINGS_WITH_NULL_OVERRIDES, 'cover');
+
+            expect(result.current.isLoading).toEqual(true);
+
+            await waitFor(() => {
+                expect(result.current.isLoading).toEqual(false);
+                expect(result.current.pageTemplateSettings).toEqual({
+                    ...PAGE_SETTINGS,
+                    ...THEME_SETTINGS,
+                });
+                expect(result.current.customOverrides).toEqual([]);
+            });
+        });
     });
 });
--- packages/app-bridge/src/react/usePageTemplateSettings.ts
+++ packages/app-bridge/src/react/usePageTemplateSettings.ts
@@ -4,14 +4,19 @@ import { useEffect, useState } from 'react';
 
 import type { AppBridgeTheme } from '../AppBridgeTheme';
 import type { EmitterEvents } from '../types';
+import { useThemeSettings } from './';
 
 export const usePageTemplateSettings = <TPageTemplateSettings = Record<string, unknown>>(
     appBridge: AppBridgeTheme,
     template: 'cover' | 'documentPage' | 'library',
     documentOrDocumentPageId?: number,
 ) => {
+    const { themeSettings } = useThemeSettings(appBridge);
     const [pageTemplateSettings, setPageTemplateSettings] = useState<Nullable<TPageTemplateSettings>>(null);
-    const [isLoading, setIsLoading] = useState<boolean>(true);
+    const [isLoading, setIsLoading] = useState(false);
+    const [loadSettingsError, setLoadSettingsError] = useState(false);
+    const [customOverrides, setCustomOverrides] = useState<string[]>([]);
+    const [themeFieldsWhenOverrideIsNull, setThemeFieldsWhenOverrideIsNull] = useState<Record<string, unknown>>({});
 
     useEffect(() => {
         const updateBlockSettingsFromEvent = (event: EmitterEvents['AppBridge:PageTemplateSettingsUpdated']) => {
@@ -19,33 +24,30 @@ export const usePageTemplateSettings = <TPageTemplateSettings = Record<string, u
         };
 
         const getInitialPageTemplateSettings = async () => {
+            if (template !== 'cover' && documentOrDocumentPageId === undefined) {
+                console.error(`Document ID is required for ${template} template settings`);
+                setLoadSettingsError(true);
+                return;
+            }
+
             setIsLoading(true);
+            let loadedSettings: Nullable<TPageTemplateSettings> = null;
 
             if (template === 'cover') {
-                const coverPageSettings = await appBridge.getCoverPageTemplateSettings<TPageTemplateSettings>();
-                setPageTemplateSettings(coverPageSettings);
+                loadedSettings = await appBridge.getCoverPageTemplateSettings<TPageTemplateSettings>();
             } else if (template === 'documentPage') {
-                if (documentOrDocumentPageId === undefined) {
-                    console.error('Document ID is required for document page template settings');
-                } else {
-                    const documentSettings =
-                        await appBridge.getDocumentPageTemplateSettings<TPageTemplateSettings>(
-                            documentOrDocumentPageId,
-                        );
-
-                    setPageTemplateSettings(documentSettings);
-                }
+                loadedSettings = await appBridge.getDocumentPageTemplateSettings<TPageTemplateSettings>(
+                    documentOrDocumentPageId as number,
+                );
             } else if (template === 'library') {
-                if (documentOrDocumentPageId === undefined) {
-                    console.error('Document ID is required for library template settings');
-                } else {
-                    const librarySettings =
-                        await appBridge.getLibraryPageTemplateSettings<TPageTemplateSettings>(documentOrDocumentPageId);
-                    setPageTemplateSettings(librarySettings);
-                }
+                loadedSettings = await appBridge.getLibraryPageTemplateSettings<TPageTemplateSettings>(
+                    documentOrDocumentPageId as number,
+                );
             }
 
             setIsLoading(false);
+            setLoadSettingsError(false);
+            setPageTemplateSettings(loadedSettings);
         };
 
         getInitialPageTemplateSettings();
@@ -56,6 +58,38 @@ export const usePageTemplateSettings = <TPageTemplateSettings = Record<string, u
             window.emitter.off('AppBridge:PageTemplateSettingsUpdated', updateBlockSettingsFromEvent);
         };
     }, [appBridge, documentOrDocumentPageId, template]);
+
+    useEffect(() => {
+        if (!themeSettings || !pageTemplateSettings) {
+            return;
+        }
+
+        for (const field of Object.keys(themeSettings)) {
+            if (!pageTemplateSettings.hasOwnProperty(field)) {
+                continue;
+            }
+
+            if (
+                (pageTemplateSettings as Record<string, unknown>)[field] !== null &&
+                (pageTemplateSettings as Record<string, unknown>)[field] !== undefined
+            ) {
+                setCustomOverrides((current) => (current.includes(field) ? current : [...current, field]));
+                setThemeFieldsWhenOverrideIsNull((current) => ({
+                    ...Object.fromEntries(
+                        Object.keys(current)
+                            .filter((key) => key !== field)
+                            .map((key) => [key, current[key]]),
+                    ),
+                }));
+            } else {
+                setThemeFieldsWhenOverrideIsNull((current) => ({
+                    ...current,
+                    [field]: themeSettings[field],
+                }));
+                setCustomOverrides((current) => current.filter((override) => override !== field));
+            }
+        }
+    }, [themeSettings, pageTemplateSettings]);
 
     const updatePageTemplateSettings = async (pageTemplateSettingsUpdate: Partial<TPageTemplateSettings>) => {
         try {
@@ -91,5 +125,16 @@ export const usePageTemplateSettings = <TPageTemplateSettings = Record<string, u
         }
     };
 
-    return { pageTemplateSettings, updatePageTemplateSettings, isLoading };
+    return {
+        pageTemplateSettings: loadSettingsError
+            ? null
+            : {
+                  ...themeSettings,
+                  ...pageTemplateSettings,
+                  ...themeFieldsWhenOverrideIsNull,
+              },
+        customOverrides,
+        updatePageTemplateSettings,
+        isLoading,
+    };
 };
