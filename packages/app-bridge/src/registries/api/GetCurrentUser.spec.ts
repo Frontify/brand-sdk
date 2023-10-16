3ef3ee9decd97619903c212e519c2d5302b7281a
Julian Iff <julian.iff@frontify.com>
2023-10-16 16:17:18 +0200

    Update packages/app-bridge/src/registries/api/GetCurrentUser.spec.ts

--- packages/app-bridge/src/registries/api/GetCurrentUser.spec.ts
+++ packages/app-bridge/src/registries/api/GetCurrentUser.spec.ts
@@ -1,6 +1,5 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
-/* (c) Copyright Frontify Ltd., all rights reserved. */
 import { describe, expect, it } from 'vitest';
 import { getCurrentUser } from './GetCurrentUser';
 
