bec52077c52a9c03648274e5ea3caf2eac5f7c45
Florian Gächter <818120+floriangaechter@users.noreply.github.com>
2023-10-17 11:04:32 +0200

    fix: deprecated dropdown types (#604)

--- packages/cli/templates/content-block-css/src/settings.ts
+++ packages/cli/templates/content-block-css/src/settings.ts
@@ -1,4 +1,4 @@
-import { defineSettings, DropdownSize, IconEnum } from '@frontify/guideline-blocks-settings';
+import { defineSettings, IconEnum } from '@frontify/guideline-blocks-settings';
 
 export const settings = defineSettings({
     main: [
@@ -6,7 +6,7 @@ export const settings = defineSettings({
             id: 'main-dropdown',
             type: 'dropdown',
             defaultValue: 'content_block',
-            size: DropdownSize.Large,
+            size: 'large',
             disabled: true,
             choices: [
                 {
