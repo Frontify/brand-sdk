2fd4caecd9f3191823b781c453ad41e039cdbfc7
julianiff <julian.iff@frontify.com>
2023-10-17 14:54:49 +0200

    chore: simplify template apps

--- packages/cli/templates/platform-app-css/src/index.tsx
+++ packages/cli/templates/platform-app-css/src/index.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
 import './main.css';
+import '@frontify/fondue/style';
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 ReactDOM.createRoot(document.getElementById('root')!).render(
--- packages/cli/templates/platform-app-tailwind/src/App.tsx
+++ packages/cli/templates/platform-app-tailwind/src/App.tsx
@@ -1,115 +1,27 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
 import { useEffect, useState } from 'react';
-import { Card, LoadingCircle, LoadingCircleSize, LoadingCircleStyle, Stack, Text } from '@frontify/fondue';
 import {
     PlatformAppContext,
-    createAsset,
-    getAssetResourceInformation,
     usePlatformAppBridge,
 } from '@frontify/app-bridge';
 
-type AssetResource = {
-    type: string;
-    id: string;
-    title?: string;
-    previewUrl?: string;
-    downloadUrl?: string;
-    filename?: string | null;
-    sourceUrl?: string;
-    html?: string;
-};
-
 export const App = () => {
     const appBridge = usePlatformAppBridge();
-
-    const [imageSrc, setImageSrc] = useState<string | undefined>(undefined);
-    const [assetRessources, setAssetRessources] = useState<AssetResource>();
-    const [context, setContext] = useState<PlatformAppContext>([]);
-    const [reUploadCounter, setReUploadCounter] = useState<number>(0);
-    const [uploading, setUploading] = useState<boolean>(false);
+    const [context, setContext] = useState<PlatformAppContext>();
 
     useEffect(() => {
-        (async () => {
-            if (!appBridge) {
-                return;
-            }
-
-            const currentContext = appBridge.context().get();
-            setContext(currentContext);
-
-            if (currentContext?.type === 'ASSET_ACTION') {
-                const assetRessources = await appBridge.api(
-                    getAssetResourceInformation({ assetId: currentContext.assetId }),
-                );
-
-                setAssetRessources(assetRessources);
-                setImageSrc(assetRessources.previewUrl);
-            }
-        })();
-    }, [appBridge]);
-
-    const onButtonClick = async () => {
-        if (!appBridge || !assetRessources?.downloadUrl) {
+        if (!appBridge) {
             return;
         }
 
-        setUploading(true);
-        await appBridge.api(
-            createAsset({
-                data: assetRessources.downloadUrl,
-                filename: 'test.jpeg',
-                parentId: appBridge?.context('parentId').get(),
-            }),
-        );
-
-        setReUploadCounter(reUploadCounter + 1);
-        setUploading(false);
-    };
+        setContext(appBridge.context().get());
+    }, [appBridge]);
 
     return (
-        <div className="flex justify-center flex-col items-center h-[100vh]">
-            <a href="https://developer.frontify.com" target="_blank" rel="noreferrer">
-                Frontify
-            </a>
-            <div className="p-4">
-                <Card>
-                    <div className="flex flex-row items-center p-3">
-                        <div className="w-[550px]">
-                            <Stack spacing={8} padding={16} margin={4}>
-                                <div className="self-center">
-                                    <Text size="large">Frontify Platform Apps</Text>
-                                </div>
-                                <Text>
-                                    <b>Entry Point</b>: {appBridge?.context('type').get()}
-                                </Text>
-                                <div>
-                                    <Text>
-                                        <b>Context</b>:
-                                    </Text>{' '}
-                                    {Object.keys(context).map((item) => (
-                                        <Text key={item}>{item} </Text>
-                                    ))}
-                                </div>
-                            </Stack>
-                        </div>
-                        {imageSrc && <img src={imageSrc} alt="alt-title" className="h-36" />}
-                    </div>
-                </Card>
-            </div>
-
-            {context?.type === 'ASSET_ACTION' && (
-                <button className="bg-black rounded py-1 px-3" onClick={onButtonClick}>
-                    <div className="flex items-center text-white">
-                        {reUploadCounter > 0 ? `re-uploaded ${reUploadCounter} times` : 'Reupload Image'}
-                        {uploading && (
-                            <div className="pl-2">
-                                <LoadingCircle size={LoadingCircleSize.Small} style={LoadingCircleStyle.Progress} />
-                            </div>
-                        )}
-                    </div>
-                </button>
-            )}
+        <div className="flex h-[100vh] justify-center items-center flex-col">
+            A Frontify Platform App in React with pure CSS.
+            <p className={"text-blue-500"}>Entrypoint: {context?.type}</p>.
         </div>
     );
 };
