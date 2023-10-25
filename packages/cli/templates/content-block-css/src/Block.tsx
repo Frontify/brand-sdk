b571cbf86a0d0ebc47aead17bb51edc407dd3196
Florian Gächter <818120+floriangaechter@users.noreply.github.com>
2023-10-25 12:00:56 +0200

    fix: remove typo and move eslint ignore information to `.eslintrc` file (#614)
    * fix: move ingore info to .eslintrc
    
    * fix: typo
    
    * chore: add changeset
    
    * chore: lint fix
    
    ---------
    
    Co-authored-by: Samuel Alev <samuel.alev@frontify.com>

--- packages/cli/templates/content-block-css/src/Block.tsx
+++ packages/cli/templates/content-block-css/src/Block.tsx
@@ -1,8 +1,17 @@
 import type { FC } from 'react';
+import { useBlockSettings } from '@frontify/app-bridge';
 import type { BlockProps } from '@frontify/guideline-blocks-settings';
 
 import './style.css';
 
+type Settings = {
+    'main-dropdown': 'content_block';
+};
+
 export const AnExampleBlock: FC<BlockProps> = ({ appBridge }) => {
+    const [blockSettings] = useBlockSettings<Settings>(appBridge);
+
+    console.log(blockSettings);
+
     return <span className="block-text">A custom block in violet and underlined</span>;
 };
