cda39e9fb55c2ab3233dc2bfc6aed93c90f7e89e
julianiff <julian.iff@frontify.com>
2023-10-25 17:25:52 +0200

    fix: usePlatformApp test

--- packages/app-bridge/src/react/usePlatformAppBridge.spec.ts
+++ packages/app-bridge/src/react/usePlatformAppBridge.spec.ts
@@ -9,7 +9,12 @@ describe('usePlatformAppBridge', () => {
 
     window.location.search = `?token=${TOKEN}`;
     vi.mock('../utilities/subscribe', () => ({
-        subscribe: vi.fn().mockResolvedValue({ port: { onmessage: vi.fn() }, test: 'passed' }),
+        subscribe: vi.fn().mockResolvedValue({
+            stateIframePort: { onmessage: vi.fn() },
+            apiIframePort: { onmessage: vi.fn() },
+            context: { parentId: 'parentId-test', connected: true },
+            state: { settings: 'settings-test' },
+        }),
     }));
 
     vi.mock('../utilities/notify', () => ({
