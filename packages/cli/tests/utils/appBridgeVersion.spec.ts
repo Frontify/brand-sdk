e984617e5f94e77cc7e700fd62844c51462e1260
ragi96 <rafael.giezendanner@frontify.com>
2023-11-06 11:34:06 +0100

    test: improve appBridgeVersion tests

--- packages/cli/tests/utils/appBridgeVersion.spec.ts
+++ packages/cli/tests/utils/appBridgeVersion.spec.ts
@@ -1,12 +1,43 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
-import { describe, expect, it } from 'vitest';
+import mockFs from 'mock-fs';
+import { afterEach, describe, expect, it } from 'vitest';
 import { getAppBridgeVersion } from '../../src/utils/appBridgeVersion.js';
 
-const rootPath = `${__dirname}/../files/compile-test-files`;
+const rootPath = 'frontify-cli';
 
 describe('AppBridgeVersion utils', async () => {
-    it('should return the correct major version from package.json', () => {
-        expect(getAppBridgeVersion(rootPath)).toEqual('^3.0.0-beta.99');
+    afterEach(() => {
+        mockFs.restore();
+    });
+
+    it('should return the 3 as version from package.json', () => {
+        mockFs({
+            'frontify-cli': {
+                'package.json': JSON.parse('{"dependencies": {"@frontify/app-bridge": "3"}'),
+            },
+        });
+
+        expect(getAppBridgeVersion(rootPath)).toEqual('3');
+    });
+
+    it('should return the 3.0.0-beta.99 as version from package.json', () => {
+        mockFs({
+            'frontify-cli': {
+                'package.json': JSON.parse('{"dependencies": {"@frontify/app-bridge": "3.0.0-beta.99"}'),
+            },
+        });
+
+        expect(getAppBridgeVersion(rootPath)).toEqual('3.0.0-beta.99');
+    });
+
+    it('should return the ^3.0.0 as version from package.json', () => {
+        mockFs({
+            'frontify-cli': {
+                'package.json': JSON.parse('{"dependencies": {"@frontify/app-bridge": "^3.0.0"}'),
+            },
+        });
+
+        expect(getAppBridgeVersion(rootPath)).toEqual('^3.0.0');
     });
 });
