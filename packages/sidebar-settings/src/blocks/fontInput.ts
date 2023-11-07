2c7adacb6e8fec776f78760c4d23fb91842f1653
Anxo botana <angel.botana@frontify.com>
2023-11-07 09:05:50 +0100

    more strict typing

--- packages/sidebar-settings/src/blocks/fontInput.ts
+++ packages/sidebar-settings/src/blocks/fontInput.ts
@@ -382,14 +382,15 @@ export type FontInputBlock<AppBridge> = {
     placeholder?: string;
 
     /**
-     * Choice values, for example:
+     * Choice values:
      *    highlightPaddingChoiceValues: choices values for padding sizes choices
      *    borderRadiusChoiceValues: choices values for border radius choices
      *    aligmentChoiceValues: text alignment choices
      */
-    predefinedChoiceValues?: {
-        [choice: string]: FontInputBlockChoice[];
-    };
+    predefinedChoiceValues?: Record<
+        'highlightPaddingChoiceValues' | 'borderRadiusChoiceValues' | 'aligmentChoiceValues',
+        FontInputBlockChoice[]
+    >;
 
     /**
      * `Default` tab will be always visible if there are fields enabled and defined in the defaultValues fontInput block.
