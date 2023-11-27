bc3f8f68438c85940abc2040b5cec0bef3130f03
Mike Trummer <mike.trummer@frontify.com>
2023-11-27 18:33:37 +0100

    feat: change PortalI18nLanguage type

--- packages/app-bridge/src/types/Portal.ts
+++ packages/app-bridge/src/types/Portal.ts
@@ -1,8 +1,8 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
-export type PortalI18nLanguage = {
-    language: string;
-    label: string;
-    default: boolean;
-    draft: boolean;
+export type Language = {
+    isoCode: string;
+    name: string;
+    isDefault: boolean;
+    isDraft: boolean;
 };
