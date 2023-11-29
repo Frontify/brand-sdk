b668cd10c38568ddfe834818ae320541264b1ad9
Utkarsh Mishra <utkarsh.mishra@frontify.com>
2023-11-29 11:10:03 +0100

    Use type based import
    Co-authored-by: Samuel Alev <samuel.alev@frontify.com>

--- packages/app-bridge/src/registries/api/GetAssetBulkDownloadToken.ts
+++ packages/app-bridge/src/registries/api/GetAssetBulkDownloadToken.ts
@@ -1,6 +1,6 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
-import { Asset } from '../../types';
+import type { Asset } from '../../types';
 
 export type GetAssetBulkDownloadTokenPayload = {
     documentBlockId: number;
