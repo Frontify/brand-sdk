8f2de1c38f1e48e91086d77d322ce40bf08a843c
Samuel Alev <samuel.alev@frontify.com>
2023-11-30 11:42:17 +0100

    fix: remove unused subscribe

--- packages/app-bridge/src/react/useCoverPage.ts
+++ packages/app-bridge/src/react/useCoverPage.ts
@@ -57,11 +57,9 @@ export const useCoverPage = (
         };
 
         window.emitter.on('AppBridge:GuidelineCoverPage:Action', updateCoverPageFromEvent);
-        const unsubscribeCurrentLanguage = appBridge.context('currentLanguage').subscribe(fetchCoverPage);
 
         return () => {
             window.emitter.off('AppBridge:GuidelineCoverPage:Action', updateCoverPageFromEvent);
-            unsubscribeCurrentLanguage();
         };
     }, [appBridge, fetchCoverPage]);
 
