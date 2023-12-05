882a0ad0d9fa0840bb0adfdf1b21f1eac84c6568
julianiff <julian.iff@frontify.com>
2023-12-05 16:47:42 +0100

    chore: remove verify-manifest cli command

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -15,16 +15,8 @@ import {
     loginUser,
     logoutUser,
 } from './commands/index.js';
-import {
-    Logger,
-    compileBlock,
-    compilePlatformApp,
-    getValidInstanceUrl,
-    isValidName,
-    reactiveJson,
-} from './utils/index.js';
+import { compileBlock, compilePlatformApp, getValidInstanceUrl, isValidName, reactiveJson } from './utils/index.js';
 import pkg from '../package.json';
-import { platformAppManifestSchemaV1, verifyManifest } from './utils/verifyManifest.js';
 
 const cli = cac(pkg.name.split('/')[1]);
 
@@ -124,24 +116,6 @@ for (const appType of ['block', 'theme']) {
             );
         });
 }
-
-cli.command('verify-manifest', 'verify the manifest structure to be correct')
-    .option('--appType [appType], --app-type', '[string] specify app type. Overrides manifest values')
-    .action(async (options) => {
-        const manifest = reactiveJson<AppManifest>(join(process.cwd(), 'manifest.json'));
-        const appType = options.appType || manifest.appType;
-
-        if (appType === 'platform-app') {
-            if (manifest.metadata?.version === 1) {
-                const validatedManifest = await verifyManifest(manifest, platformAppManifestSchemaV1);
-                validatedManifest && Logger.success('Your manifest.json is valid');
-            } else {
-                Logger.error('Please specify a manifest metadata.version');
-            }
-        } else {
-            Logger.error('AppType is required.');
-        }
-    });
 
 cli.command('deploy', 'deploy the app to the marketplace')
     .option('-e, --entryPath <entryPath>', '[string] path to the entry file', { default: join('src', 'index.ts') })
