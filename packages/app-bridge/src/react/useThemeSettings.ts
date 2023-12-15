63f7dbe12a292b497021e5c46213329cebfe2cc7
Anxo botana <angel.botana@frontify.com>
2023-12-15 11:49:11 +0100

    typing

--- packages/app-bridge/src/react/useThemeSettings.ts
+++ packages/app-bridge/src/react/useThemeSettings.ts
@@ -2,10 +2,10 @@
 
 import { useEffect, useState } from 'react';
 
-import type { AppBridgeTheme } from '../AppBridgeTheme';
+import type { AppBridgeTheme, ThemeTemplate } from '../AppBridgeTheme';
 import type { EmitterEvents } from '../types';
 
-export const useThemeSettings = <T = Record<string, unknown>>(appBridge: AppBridgeTheme) => {
+export const useThemeSettings = <T = Record<ThemeTemplate, unknown>>(appBridge: AppBridgeTheme) => {
     const [themeSettings, setThemeSettings] = useState<Nullable<T>>(null);
     const [isLoading, setIsLoading] = useState<boolean>(true);
 
