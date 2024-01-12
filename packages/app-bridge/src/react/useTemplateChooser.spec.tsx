14d80463bfd3011e4cdf070a5d8212c772a5c664
ragi96 <rafael.giezendanner@frontify.com>
2024-01-12 11:14:24 +0100

    test: improve LegacyTemplateDummy import

--- packages/app-bridge/src/react/useTemplateChooser.spec.tsx
+++ packages/app-bridge/src/react/useTemplateChooser.spec.tsx
@@ -9,7 +9,7 @@ import { TemplateLegacy } from '../types/TemplateLegacy';
 import { AppBridgeBlock } from '../AppBridgeBlock';
 import { useTemplateChooser } from './useTemplateChooser';
 import { withAppBridgeBlockStubs } from '../tests/withAppBridgeBlockStubs';
-import { TemplateLegacyDummy } from '..';
+import { TemplateLegacyDummy } from '../tests/TemplateLegacyDummy';
 
 const OPEN_TEMPLATE_CHOOSER_BUTTON_ID = 'open-template-chooser';
 const CLOSE_TEMPLATE_CHOOSER_BUTTON_ID = 'close-template-chooser';
