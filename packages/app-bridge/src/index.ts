d00eb2e290c778234b1a81fea10de8b572d8604f
Samuel Alev <samuel.alev@frontify.com>
2024-01-22 17:04:13 +0100

    refactor: split testing utilities out of main bundle

--- packages/app-bridge/src/index.ts
+++ packages/app-bridge/src/index.ts
@@ -13,6 +13,5 @@ export * from './errors';
 export * from './helpers';
 export * from './registries';
 export * from './react';
-export * from './tests';
 export * from './types';
 export * from './utilities';
