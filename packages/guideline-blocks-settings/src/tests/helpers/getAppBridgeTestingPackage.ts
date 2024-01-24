66a3899b597f32510e7b7f16bd9905a31b216f7a
Samuel Alev <samuel.alev@frontify.com>
2024-01-24 20:44:37 +0100

    update

--- packages/guideline-blocks-settings/src/tests/helpers/getAppBridgeTestingPackage.ts
+++ packages/guideline-blocks-settings/src/tests/helpers/getAppBridgeTestingPackage.ts
@@ -12,7 +12,7 @@ type GetMinorVersion<T extends string> = T extends `${infer _}.${infer Minor}.${
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 type GetPatchVersion<T extends string> = T extends `${infer _}.${infer _}.${infer Patch}` ? Patch : never;
 
-// @ts-ignore will fail if <=v3.0.3 is installed
+// @ts-ignore will fail if <=v3.0.4 is installed
 type TypedPackageJson = typeof import('../../../node_modules/@frontify/app-bridge/package.json.d.ts');
 
 const appBridgeVersion = packageJson.version as 0 extends 1 & TypedPackageJson['version']
@@ -25,26 +25,31 @@ type AppBridgePatchVersion = GetPatchVersion<typeof appBridgeVersion>;
 
 export const getAppBridgeTestingPackage = async (): AppBridgeMajorVersion extends '3'
     ? AppBridgeMinorVersion extends '0'
-        ? AppBridgePatchVersion extends '0' | '1' | '2' | '3'
+        ? AppBridgePatchVersion extends '0' | '1' | '2' | '3' | '4'
             ? Promise<typeof import('@frontify/app-bridge')>
-            : // @ts-ignore will fail if <=v3.0.3 is installed
+            : // @ts-ignore will fail if <=v3.0.4 is installed
               Promise<typeof import('@frontify/app-bridge/testing')>
-        : // @ts-ignore will fail if <=v3.0.3 is installed
+        : // @ts-ignore will fail if <=v3.0.4 is installed
           Promise<typeof import('@frontify/app-bridge/testing')>
-    : // @ts-ignore will fail if <=v3.0.3 is installed
+    : // @ts-ignore will fail if <=v3.0.4 is installed
       Promise<typeof import('@frontify/app-bridge/testing')> => {
-    if (appBridgeVersion.startsWith('3.0.') && ['0', '1', '2', '3'].includes(appBridgeVersion.split('.')[2])) {
+    const isAppBridgeVersionWithTestingExport =
+        appBridgeVersion.startsWith('3.0.') && ['0', '1', '2', '3'].includes(appBridgeVersion.split('.')[2]);
+
+    if (isAppBridgeVersionWithTestingExport) {
         const appBridgePackage = await import('@frontify/app-bridge');
 
-        // @ts-ignore will fail if >3.0.3 is installed
+        // @ts-ignore will fail if >3.0.4 is installed
         return appBridgePackage as typeof import('@frontify/app-bridge');
     }
 
-    // Needed if <=v3.0.3 is installed as it doesn't have the /testing export
+    // Needed if <=v3.0.4 is installed as it doesn't have the /testing export
     const getPath = () => '@frontify/app-bridge/testing';
-    // @ts-ignore will fail if <=v3.0.3 is installed
-    const appBridgeTestingPackage = await import(/* @vite-ignore */ getPath());
+    // @ts-ignore will fail if <=v3.0.4 is installed
+    const appBridgeTestingPackage = await import(
+        isAppBridgeVersionWithTestingExport ? getPath() : '@frontify/app-bridge/testing'
+    );
 
-    // @ts-ignore will fail if <=v3.0.3 is installed
+    // @ts-ignore will fail if <=v3.0.4 is installed
     return appBridgeTestingPackage as typeof import('@frontify/app-bridge/testing');
 };
