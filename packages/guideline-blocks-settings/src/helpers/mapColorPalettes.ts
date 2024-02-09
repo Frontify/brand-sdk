c4f6d4b9d05fb2ec6148397318ac3f35f136d84d
ragi96 <rafael.giezendanner@frontify.com>
2024-02-09 11:27:41 +0100

    feat: support new v4 color type

--- packages/guideline-blocks-settings/src/helpers/mapColorPalettes.ts
+++ packages/guideline-blocks-settings/src/helpers/mapColorPalettes.ts
@@ -4,66 +4,61 @@ import { ColorPalette } from '@frontify/app-bridge';
 import { Palette } from '@frontify/fondue';
 import { Nullable } from 'vitest';
 
-function isNewColorType(color: any): boolean {
-    return color.color && color.color.revision;
-}
+type V3Color = {
+    id: number;
+    name: Nullable<string>;
+    red: Nullable<number>;
+    green: Nullable<number>;
+    blue: Nullable<number>;
+    alpha: Nullable<number>;
+};
+
+type V4Color = {
+    id: number;
+    color: {
+        title: Nullable<string>;
+        revision: {
+            red: Nullable<number>;
+            green: Nullable<number>;
+            blue: Nullable<number>;
+            alpha: Nullable<number>;
+        };
+    };
+};
+
+type Color = V3Color | V4Color;
 
 export const mapAppBridgeColorPalettesToFonduePalettes = (colorPalettes: ColorPalette[]): Palette[] => {
     return colorPalettes.map(mapAppBridgeColorPaletteToFonduePalette);
 };
-
 export const mapAppBridgeColorPaletteToFonduePalette = (colorPalette: ColorPalette): Palette => {
-    if (isNewColorType(colorPalette)) {
-        return mapColorType(colorPalette);
-    }
-
-    return mapLegacyColorType(colorPalette);
-};
-
-const mapColorType = (colorPalette: ColorPalette) => {
     return {
         id: colorPalette.id,
         title: colorPalette.name,
-        colors: colorPalette.colors.map((color) => ({
+        colors: colorPalette.colors.map(mapColor),
+    };
+};
+
+const isNewColor = (color: Color): color is V4Color => {
+    return (color as V4Color).color !== undefined;
+};
+
+const mapColor = (color: Color) => {
+    if (isNewColor(color)) {
+        return {
             alpha: color.color.revision.alpha ? color.color.revision.alpha / 255 : 1,
             red: color.color.revision.red ?? 0,
             green: color.color.revision.green ?? 0,
             blue: color.color.revision.blue ?? 0,
             name: color.color.title ?? '',
-        })),
-    };
-};
-
-type ColorTypeGuard = {
-    color: {
-        title: Nullable<string>;
-        revision: {
-            alpha: Nullable<number>;
-            red: Nullable<number>;
-            green: Nullable<number>;
-            blue: Nullable<number>;
         };
-    };
-};
-
-type LegacyColorTypeGuard = {
-    title: Nullable<string>;
-    alpha: Nullable<number>;
-    red: Nullable<number>;
-    green: Nullable<number>;
-    blue: Nullable<number>;
-};
-
-const mapLegacyColorType = (colorPalette: ColorPalette) => {
-    return {
-        id: colorPalette.id,
-        title: colorPalette.name,
-        colors: colorPalette.colors.map((color) => ({
+    } else {
+        return {
             alpha: color.alpha ? color.alpha / 255 : 1,
             red: color.red ?? 0,
             green: color.green ?? 0,
             blue: color.blue ?? 0,
             name: color.name ?? '',
-        })),
-    };
+        };
+    }
 };
