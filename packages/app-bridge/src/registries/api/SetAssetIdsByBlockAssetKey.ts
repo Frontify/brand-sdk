46895335d6d16365f9cbb3dfb9b7e4221e2c8da4
ragi96 <rafael.giezendanner@frontify.com>
2024-02-13 10:56:32 +0100

    fix: response type

--- packages/app-bridge/src/registries/api/SetAssetIdsByBlockAssetKey.ts
+++ packages/app-bridge/src/registries/api/SetAssetIdsByBlockAssetKey.ts
@@ -5,7 +5,7 @@ export type SetAssetIdsByBlockAssetKeyPayload = {
     assetIds: number[];
 };
 
-export type SetAssetIdsByBlockAssetKeyResponse = Promise<void>;
+export type SetAssetIdsByBlockAssetKeyResponse = void;
 
 export const setAssetIdsByBlockAssetKey = (
     payload: SetAssetIdsByBlockAssetKeyPayload,
