dcb1856d7ae08e4f2c19c0101d7cd030d6038fc5
Rafael Giezendanner <rafael.giezendanner@frontify.com>
2024-02-23 17:26:39 +0100

    fix: revert #786 and #785 (#787)
    * Revert "fix: adjust templates (#786)"
    
    This reverts commit 99aa6379da5a2608bd3ac9aef4e022799ed0871d.
    
    * Revert "chore: update to 4 and downgrade to 0.0.5 (#785)"
    
    This reverts commit 524432c8ea9f4b2e0793b17ff2efff7fff00f532.

--- packages/cli/templates/platform-app-tailwind/src/index.ts
+++ packages/cli/templates/platform-app-tailwind/src/index.ts
@@ -1,8 +1,8 @@
 import { App } from './App';
 import { defineApp } from '@frontify/platform-app';
 import { settings } from './settings';
-import '@frontify/fondue/style';
 import 'tailwindcss/tailwind.css';
+import '@frontify/fondue/style';
 import './main.css';
 
 export default defineApp({
