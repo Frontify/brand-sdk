efde7a511b3d4fbbe17af5f0e41982d003743b35
Ilham Moutaharik <ilham.moutaharik@frontify.com>
2024-03-19 09:08:44 +0100

    feat(appBridge): Add proxyNetworkCall method (#821)
    * feat(PlatformApp): add new API method
    
    * Update .changeset/clever-gifts-join.md
    
    Co-authored-by: Samuel Alev <samuel.alev@frontify.com>
    
    ---------
    
    Co-authored-by: Samuel Alev <samuel.alev@frontify.com>

---
+++ packages/app-bridge/src/registries/api/GetProxyNetworkCall.spec.ts
@@ -0,0 +1,16 @@
+/* (c) Copyright Frontify Ltd., all rights reserved. */
+
+import { describe, expect, it } from 'vitest';
+import { getProxyNetworkCall } from './GetProxyNetworkCall';
+
+describe('GetProxyNetworkCall', () => {
+    it('should return correct method name', () => {
+        const TEST_ID = 'user-api';
+        const proxyNetworkCall = getProxyNetworkCall({ id: TEST_ID, props: 'data' });
+        expect(proxyNetworkCall.name).toBe('getProxyNetworkCall');
+        expect(proxyNetworkCall.payload).toStrictEqual({
+            id: TEST_ID,
+            props: 'data',
+        });
+    });
+});
