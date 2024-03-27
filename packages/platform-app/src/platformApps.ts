1922214f7772a977d5f0621e3a2b2424ba3cc76c
julianiff <julian.iff@frontify.com>
2024-03-27 09:42:50 +0100

    chore: reformat imports

--- packages/platform-app/src/platformApps.ts
+++ packages/platform-app/src/platformApps.ts
@@ -1,36 +1,48 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
 import {
-    ChecklistBlock,
-    ColorInputBlock,
-    DropdownBlock,
+    type ChecklistBlock as ChecklistBlockSettings,
+    type ColorInputBlock as ColorInputBlockSettings,
+    type DropdownBlock as DropdownBlockSettings,
     DynamicSettingBlock,
-    InputBlock,
-    MultiInputBlock,
-    NotificationBlock,
-    SectionHeadingBlock,
-    SegmentedControlsBlock,
-    SwitchBlock,
-    TextareaBlock,
+    type InputBlock as InputBlockSettings,
+    type MultiInputBlock as MultiInputBlockSettings,
+    type NotificationBlock as NotificationBlockSettings,
+    type SectionHeadingBlock as SectionHeadingBlockSettings,
+    type SegmentedControlsBlock as SegmentedControlsBlockSettings,
+    type SettingBlock as SettingBlockSettings,
+    type SwitchBlock as SwitchBlockSettings,
+    type TextareaBlock as TextareaBlockSettings,
 } from '@frontify/sidebar-settings';
+
 import { type FC } from 'react';
-import { AppBridgePlatformApp } from '@frontify/app-bridge';
+import { type AppBridgePlatformApp } from '@frontify/app-bridge';
 
-export type PlatformAppSimpleBlock<AppBridge> =
-    | ColorInputBlock<AppBridge>
-    | ChecklistBlock<AppBridge>
-    | DropdownBlock<AppBridge>
-    | InputBlock<AppBridge>
-    | MultiInputBlock<AppBridge>
-    | NotificationBlock<AppBridge>
-    | SectionHeadingBlock<AppBridge>
-    | SegmentedControlsBlock<AppBridge>
-    | SwitchBlock<AppBridge>
-    | TextareaBlock<AppBridge>;
+export type ChecklistBlock = ChecklistBlockSettings<AppBridgePlatformApp>;
+export type ColorInputBlock = ColorInputBlockSettings<AppBridgePlatformApp>;
+export type DropdownBlock = DropdownBlockSettings<AppBridgePlatformApp>;
+export type InputBlock = InputBlockSettings<AppBridgePlatformApp>;
+export type MultiInputBlock = MultiInputBlockSettings<AppBridgePlatformApp>;
+export type NotificationBlock = NotificationBlockSettings<AppBridgePlatformApp>;
+export type SectionHeadingBlock = SectionHeadingBlockSettings<AppBridgePlatformApp>;
+export type SegmentedControlsBlock = SegmentedControlsBlockSettings<AppBridgePlatformApp>;
+export type SettingBlock = SettingBlockSettings<AppBridgePlatformApp>;
+export type SwitchBlock = SwitchBlockSettings<AppBridgePlatformApp>;
+export type TextareaBlock = TextareaBlockSettings<AppBridgePlatformApp>;
 
-export type PlatformAppSettings =
-    | PlatformAppSimpleBlock<AppBridgePlatformApp>
-    | DynamicSettingBlock<AppBridgePlatformApp>;
+export type PlatformAppSimpleBlock =
+    | ColorInputBlock
+    | ChecklistBlock
+    | DropdownBlock
+    | InputBlock
+    | MultiInputBlock
+    | NotificationBlock
+    | SectionHeadingBlock
+    | SegmentedControlsBlock
+    | SwitchBlock
+    | TextareaBlock;
+
+export type PlatformAppSettings = PlatformAppSimpleBlock | DynamicSettingBlock<AppBridgePlatformApp>;
 
 export type PlatformAppSettingsStructureExport = { [customSectionName: string]: PlatformAppSettings[] };
 
