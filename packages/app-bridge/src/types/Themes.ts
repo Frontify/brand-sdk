80b7e8a7f4601394d38292e89c9e77dc0fcead68
Mike Trummer <mike.trummer@frontify.com>
2024-04-09 11:15:18 +0200

    feat: add themes types

---
+++ packages/app-bridge/src/types/Themes.ts
@@ -0,0 +1,67 @@
+/* (c) Copyright Frontify Ltd., all rights reserved. */
+
+import { LinkSettingsDisplay, LinkSettingsIconPosition } from './Document.ts';
+
+export interface CoverPage {
+    id(): number;
+    title(language?: string): string;
+    isPublished(): boolean;
+}
+
+export interface DocumentGroup {
+    id(): number;
+    name(language?: string): string;
+    children(): Document[] | DocumentLibrary[] | DocumentLink[];
+}
+
+export interface Document {
+    id(): number;
+    title(language?: string): string;
+    slug(language?: string): string;
+    url(language?: string): string;
+}
+
+export interface DocumentLibrary {
+    id(): number;
+    title(language?: string): string;
+    slug(language?: string): string;
+    url(language?: string): string;
+}
+
+export interface DocumentLink {
+    id(): number;
+    title(language?: string): string;
+    url(): string;
+    displayMode(): LinkSettingsDisplay;
+    iconPosition(): LinkSettingsIconPosition;
+    customIconUrl(): Nullable<string>;
+    shouldOpenInNewTab(): boolean;
+}
+
+export interface PageCategory {
+    id(): number;
+    title(language?: string): string;
+    slug(language?: string): string;
+    children(): DocumentPage[] | DocumentPageLink[];
+}
+
+export interface DocumentPage {
+    id(): number;
+    title(language?: string): string;
+    slug(language?: string): string;
+    url(language?: string): string;
+    headings(): DocumentPageHeading[];
+}
+
+export interface DocumentPageLink {
+    id(): number;
+    title(language?: string): string;
+    url(): string;
+    shouldOpenInNewTab(): boolean;
+}
+
+export interface DocumentPageHeading {
+    id(): number;
+    title(language?: string): string;
+    slug(language?: string): string;
+}
