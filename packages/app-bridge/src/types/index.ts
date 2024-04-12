2ecec711ac717744da8d95e1f1daf712a88f587c
Mike Trummer <mike.trummer@frontify.com>
2024-04-12 16:29:47 +0200

    feat: add themes types (#854)
    * feat: add themes types
    
    * fix: types
    
    * refactor: remove shouldOpenInNewTab from DocumentPageLink
    
    * refactor: rename name to title
    
    * feat: add isHiddenInNavigation and url to CoverPage
    
    * refactor: prefix types with Guidelines
    
    * feat: add type property
    
    * feat: add parentId to interfaces
    
    * feat: make prefix singular
    
    * fix: rename type file

--- packages/app-bridge/src/types/index.ts
+++ packages/app-bridge/src/types/index.ts
@@ -20,6 +20,7 @@ export * from './File';
 export * from './FileExtension';
 export * from './FileExtensionSets';
 export * from './FileType';
+export * from './Guideline';
 export * from './GuidelineSearchResult';
 export * from './Language';
 export * from './Notify';
