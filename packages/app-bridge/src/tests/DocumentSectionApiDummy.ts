072af0153a4d4e3ad75f186b4a90584c0bbb7d3e
Luca Hochreutener <luca.hochreutener@frontify.com>
2024-06-05 10:28:31 +0200

    chore: remove unnecessary properties on DocumentSectionApi

--- packages/app-bridge/src/tests/DocumentSectionApiDummy.ts
+++ packages/app-bridge/src/tests/DocumentSectionApiDummy.ts
@@ -12,8 +12,6 @@ export class DocumentSectionApiDummy {
             modified: '2022-04-04T15:36:43.000+00:00',
             document_id: 1,
             page_id: 6,
-            revision: null,
-            valid_from: '2022-04-04T15:36:36.000+00:00',
             valid_to: null,
             title: `Document Section Dummy ${id}`,
             slug: `document-section-dummy-${id}`,
