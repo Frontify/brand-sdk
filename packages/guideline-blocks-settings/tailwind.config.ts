c8d081f52265fa4b0f60ca6eef905fd5d06e0ae3
fulopdaniel <daniel.fulop@frontify.com>
2024-06-05 15:41:54 +0200

    fix(*): container query plugin is not a public dependency

--- packages/guideline-blocks-settings/tailwind.config.ts
+++ packages/guideline-blocks-settings/tailwind.config.ts
@@ -1,12 +1,14 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
+import containerQueryPlugin from '@tailwindcss/container-queries';
+
 module.exports = {
     presets: [require('@frontify/fondue/tailwind')],
     content: ['src/**/*.{ts,tsx}'],
     corePlugins: {
         preflight: false,
     },
-    plugins: [require('@tailwindcss/container-queries')],
+    plugins: [containerQueryPlugin],
     theme: {
         extend: {
             colors: {
