320957899f015bb3805d554c141426409d432c4c
Anxo botana <angel.botana@frontify.com>
2024-06-18 12:39:28 +0200

    fix: appBridgeTheme fix build and codeowners

--- packages/app-bridge-theme/src/index.ts
+++ packages/app-bridge-theme/src/index.ts
@@ -1,3 +1,6 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
 export * from './AppBridgeTheme';
+export * from './types/index';
+export * from './types';
+export * from './commands';
---
+++ packages/app-bridge-theme/src/types/index.ts
@@ -0,0 +1,8 @@
+/* (c) Copyright Frontify Ltd., all rights reserved. */
+
+export * from './Asset';
+export * from './Context';
+export * from './Guideline';
+export * from './GuidelineSearchResult';
+export * from './Language';
+export * from './ThemeTemplate';
