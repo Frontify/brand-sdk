e8317e65cd9c21fb8fb22fdcbddb9a4ac5d068f2
julianiff <julian.iff@frontify.com>
2024-06-18 21:04:39 +0200

    chore: update templates

--- packages/cli/templates/platform-app-tailwind/src/App.tsx
+++ packages/cli/templates/platform-app-tailwind/src/App.tsx
@@ -1,24 +1,12 @@
-/* (c) Copyright Frontify Ltd., all rights reserved. */
-
-import { type PlatformAppContext, usePlatformAppBridge } from '@frontify/app-bridge';
-import { useEffect, useState } from 'react';
+import { AppBridgePlatformApp } from '@frontify/app-bridge';
 
 export const App = () => {
-    const appBridge = usePlatformAppBridge();
-    const [context, setContext] = useState<PlatformAppContext>();
-
-    useEffect(() => {
-        if (!appBridge) {
-            return;
-        }
-
-        setContext(appBridge.context().get());
-    }, [appBridge]);
+    const appBridge = new AppBridgePlatformApp();
 
     return (
         <div className="flex h-[100vh] justify-center items-center flex-col">
             A Frontify Platform App in React with tailwind.
-            <p className={'text-blue-500'}>Entrypoint: {context?.surface}</p>.
+            <p className={'text-blue-500'}>Entrypoint: {appBridge.context().get().surface}</p>.
         </div>
     );
 };
