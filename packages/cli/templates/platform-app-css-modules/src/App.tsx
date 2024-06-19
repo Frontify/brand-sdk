6d0c3dd22ffdf15d2c04b8fb6a57cef65e3f0001
Julian Iff <julian.iff@frontify.com>
2024-06-19 13:43:27 +0200

    chore: update templates (#968)
    * chore: update templates
    
    * Create gentle-pots-deny.md

--- packages/cli/templates/platform-app-css-modules/src/App.tsx
+++ packages/cli/templates/platform-app-css-modules/src/App.tsx
@@ -1,24 +1,14 @@
-import { type PlatformAppContext, usePlatformAppBridge } from '@frontify/app-bridge';
-import { useEffect, useState } from 'react';
+import { AppBridgePlatformApp } from '@frontify/app-bridge';
 
 import style from './style.module.css';
 
 export const App = () => {
-    const appBridge = usePlatformAppBridge();
-    const [context, setContext] = useState<PlatformAppContext>();
-
-    useEffect(() => {
-        if (!appBridge) {
-            return;
-        }
-
-        setContext(appBridge.context().get());
-    }, [appBridge]);
+    const appBridge = new AppBridgePlatformApp();
 
     return (
         <div className={style.container}>
             A Frontify Platform App in React with CSS Modules
-            <span className={style.text}>Entrypoint: {context?.surface}</span>.
+            <span className={style.text}>Entrypoint: {appBridge.context().get().surface}</span>.
         </div>
     );
 };
