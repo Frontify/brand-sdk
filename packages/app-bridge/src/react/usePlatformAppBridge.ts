3cd66e647d20e9213ecad6b503223ce95d7e52d3
julianiff <julian.iff@frontify.com>
2024-06-26 15:07:23 +0200

    chore: add deprecation notice

--- packages/app-bridge/src/react/usePlatformAppBridge.ts
+++ packages/app-bridge/src/react/usePlatformAppBridge.ts
@@ -4,6 +4,9 @@ import { useEffect, useState } from 'react';
 
 import { AppBridgePlatformApp } from '../AppBridgePlatformApp';
 
+/**
+ * @deprecated Use new AppBridgePlatformApp() directly in code. This hook is not needed anymore
+ */
 export const usePlatformAppBridge = () => {
     const [platformAppBridge, setPlatformAppBridge] = useState<AppBridgePlatformApp | undefined>();
 
