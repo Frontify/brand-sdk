36697b6a2092b7d9c330a47023bd205ab65f8730
julianiff <julian.iff@frontify.com>
2024-06-27 09:51:32 +0200

    chore: fix package

--- packages/app-bridge-app/src/index.ts
+++ packages/app-bridge-app/src/index.ts
@@ -4,3 +4,5 @@ export * from './AppBridgePlatformApp';
 export * from './errors';
 export * from './registries';
 export * from './utilities';
+export * from './react/appUserState';
+export * from './react/appSettings';
