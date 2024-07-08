5b3d8529529a79ff8ae05219340a16ed4cd1eb11
julianiff <julian.iff@frontify.com>
2024-07-08 15:11:32 +0200

    chore: adjust test

--- packages/app-bridge-app/src/registries/api/GetAccountId.spec.ts
+++ packages/app-bridge-app/src/registries/api/GetAccountId.spec.ts
@@ -5,7 +5,7 @@ import { describe, expect, it } from 'vitest';
 import { getAccountId } from './GetAccountId.ts';
 
 describe('GetAccountId', () => {
-    it('should return correct method name', () => {
+    it('should return correct method name of AccountId', () => {
         const accountIdMethod = getAccountId();
         expect(accountIdMethod.name).toBe('getAccountId');
     });
