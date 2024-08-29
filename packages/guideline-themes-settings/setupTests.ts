c6bb2e47c0003d588fddff14dad6639a9a28e6bf
Anxo botana <angel.botana@frontify.com>
2024-08-29 14:06:19 +0200

    more cleaning

--- packages/guideline-themes-settings/setupTests.ts
+++ packages/guideline-themes-settings/setupTests.ts
@@ -1,23 +1,13 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
-import * as matchers from '@testing-library/jest-dom/matchers';
-import { cleanup, configure } from '@testing-library/react';
-import { afterEach, beforeAll, expect, vi } from 'vitest';
-import '@testing-library/jest-dom/vitest';
-
-expect.extend(matchers);
+import { afterEach, vi } from 'vitest';
 
 vi.stubGlobal('crypto', {
     getRandomValues: vi.fn(),
 });
 
-beforeAll(() => {
-    configure({ testIdAttribute: 'data-test-id' });
-});
-
 afterEach(() => {
     vi.restoreAllMocks();
-    cleanup();
 });
 
 globalThis.structuredClone = (data: unknown) => JSON.parse(JSON.stringify(data));
