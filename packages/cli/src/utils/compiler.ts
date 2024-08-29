0ab60afc5d9e2faf515acbef6dc85d1a9ea1845e
julianiff <julian.iff@frontify.com>
2024-08-29 14:21:44 +0200

    chore: adjust build to be a folder

--- packages/cli/src/utils/compiler.ts
+++ packages/cli/src/utils/compiler.ts
@@ -1,9 +1,7 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
-import { createHash } from 'node:crypto';
-
 import react from '@vitejs/plugin-react';
-import { type PluginOption, build } from 'vite';
+import { build } from 'vite';
 import { viteExternalsPlugin } from 'vite-plugin-externals';
 
 import { getAppBridgeThemeVersion } from './appBridgeThemeVersion';
@@ -94,32 +92,7 @@ export const compileBlock = async ({ projectPath, entryFile, outputName }: Compi
 };
 
 export const compilePlatformApp = async ({ outputName, entryFile, projectPath = '' }: CompilerOptions) => {
-    const getHash = (text) => createHash('sha256').update(text).digest('hex');
     const appBridgeVersion = getAppBridgeVersion(projectPath);
-
-    const htmlHashPlugin: PluginOption = {
-        name: 'html-hash',
-        enforce: 'post',
-        transformIndexHtml(html, { bundle }) {
-            const indexJsSource = bundle?.['index.js'].type === 'chunk' ? bundle?.['index.js'].code : null;
-            const indexCssSource = bundle?.['index.css'].type === 'asset' ? bundle?.['index.css'].source : null;
-
-            const cssFileName = `${outputName}.${getHash(indexCssSource)}.css`;
-            const jsFileName = `${outputName}.${getHash(indexJsSource)}.js`;
-
-            html = html.replace('index.css', cssFileName).replace('index.js', jsFileName);
-            return html;
-        },
-        generateBundle(_options, bundle) {
-            const indexHtmlSource = bundle?.['index.html'].type === 'asset' ? bundle?.['index.html'].source : null;
-            const indexJsSource = bundle?.['index.js'].type === 'chunk' ? bundle?.['index.js'].code : null;
-            const indexCssSource = bundle?.['index.css'].type === 'asset' ? bundle?.['index.css'].source : null;
-
-            bundle['index.html'].fileName = `${outputName}.${getHash(indexHtmlSource)}.html`;
-            bundle['index.js'].fileName = `${outputName}.${getHash(indexJsSource)}.js`;
-            bundle['index.css'].fileName = `${outputName}.${getHash(indexCssSource)}.css`;
-        },
-    };
 
     const settings = await build({
         plugins: [
@@ -159,20 +132,14 @@ export const compilePlatformApp = async ({ outputName, entryFile, projectPath = 
     });
 
     const app = await build({
-        plugins: [react(), htmlHashPlugin],
+        plugins: [react()],
         root: projectPath,
         define: {
             'process.env.NODE_ENV': JSON.stringify('production'),
         },
+        base: '/__DYNAMIC_SEGMENT__/',
         build: {
             emptyOutDir: false,
-            rollupOptions: {
-                output: {
-                    assetFileNames: () => '[name][extname]',
-                    chunkFileNames: '[name].js',
-                    entryFileNames: '[name].js',
-                },
-            },
         },
     });
     return { app, settings };
