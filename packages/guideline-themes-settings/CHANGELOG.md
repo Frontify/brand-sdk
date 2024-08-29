7b56c9b5545e2f21e6948fcd390964d030415a3e
Anxo botana <angel.botana@frontify.com>
2024-08-29 12:08:26 +0200

    more updates

--- packages/guideline-themes-settings/CHANGELOG.md
+++
@@ -1,1 +0,0 @@
-# @frontify/guideline-themes-settings
--- packages/guideline-themes-settings/tailwind.config.ts
+++ packages/guideline-themes-settings/tailwind.config.ts
@@ -1,12 +1,9 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
-import containerQueryPlugin from '@tailwindcss/container-queries';
-
 module.exports = {
     presets: [require('@frontify/fondue/tailwind')],
     content: ['src/**/*.{ts,tsx}'],
     corePlugins: {
         preflight: false,
     },
-    plugins: [containerQueryPlugin],
 };
