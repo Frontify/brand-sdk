bc8b4144df727c048cf42c912077acf6570986ac
Julian Iff <julian.iff@frontify.com>
2024-09-04 10:55:55 +0200

    refactor: adjust folder structure for compiler (#1099)
    * chore: adjust folder structure
    
    * chore: add correct path
    
    * chore: remove whitespace
    
    * chore: cleanup naming
    
    * chore: renaming

---
+++ packages/cli/src/utils/compiler/compileBlock.ts
@@ -0,0 +1,48 @@
+/* (c) Copyright Frontify Ltd., all rights reserved. */
+
+import react from '@vitejs/plugin-react';
+import { build } from 'vite';
+import { viteExternalsPlugin } from 'vite-plugin-externals';
+
+import { getAppBridgeVersion } from '../appBridgeVersion';
+
+import { type CompilerOptions } from './compilerOptions';
+
+export const compileBlock = async ({ projectPath, entryFile, outputName }: CompilerOptions) => {
+    const appBridgeVersion = getAppBridgeVersion(projectPath);
+    return build({
+        plugins: [
+            react(),
+            viteExternalsPlugin({
+                react: 'React',
+                'react-dom': 'ReactDOM',
+            }),
+        ],
+        define: {
+            'process.env.NODE_ENV': JSON.stringify('production'),
+        },
+        root: projectPath,
+        build: {
+            lib: {
+                name: outputName,
+                entry: entryFile,
+                formats: ['iife'],
+                fileName: () => 'index.js',
+            },
+            rollupOptions: {
+                external: ['react', 'react-dom'],
+                output: {
+                    globals: {
+                        react: 'React',
+                        'react-dom': 'ReactDOM',
+                    },
+                    footer: `
+                        window.${outputName} = ${outputName};
+                        window.${outputName}.dependencies = window.${outputName}.packages || {};
+                        window.${outputName}.dependencies['@frontify/app-bridge'] = '${appBridgeVersion}';
+                    `,
+                },
+            },
+        },
+    });
+};
---
+++ packages/cli/src/utils/compiler/compilePlatformApp.ts
@@ -0,0 +1,63 @@
+/* (c) Copyright Frontify Ltd., all rights reserved. */
+
+import react from '@vitejs/plugin-react';
+import { build } from 'vite';
+import { viteExternalsPlugin } from 'vite-plugin-externals';
+
+import { getAppBridgeVersion } from '../appBridgeVersion';
+
+import { type CompilerOptions } from './compilerOptions';
+
+export const compilePlatformApp = async ({ outputName, entryFile, projectPath = '' }: CompilerOptions) => {
+    const appBridgeVersion = getAppBridgeVersion(projectPath);
+
+    const settings = await build({
+        plugins: [
+            react(),
+            viteExternalsPlugin({
+                react: 'React',
+                'react-dom': 'ReactDOM',
+            }),
+        ],
+        root: projectPath,
+        define: {
+            'process.env.NODE_ENV': JSON.stringify('production'),
+        },
+        build: {
+            lib: {
+                entry: entryFile,
+                name: outputName,
+                formats: ['iife'],
+                fileName: () => 'index.js',
+            },
+            rollupOptions: {
+                external: ['react', 'react-dom'],
+                output: {
+                    globals: {
+                        react: 'React',
+                        'react-dom': 'ReactDOM',
+                    },
+                    entryFileNames: 'settings.js',
+                    footer: `
+                        window.${outputName} = ${outputName};
+                        window.${outputName}.dependencies = window.${outputName}.packages || {};
+                        window.${outputName}.dependencies['@frontify/app-bridge'] = '${appBridgeVersion}';
+                    `,
+                },
+            },
+        },
+    });
+
+    const app = await build({
+        plugins: [react()],
+        root: projectPath,
+        define: {
+            'process.env.NODE_ENV': JSON.stringify('production'),
+        },
+        base: '/__DYNAMIC_SEGMENT__/',
+        build: {
+            emptyOutDir: false,
+        },
+    });
+    return { app, settings };
+};
