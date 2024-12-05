fc200d72fb5e4c1abae7a54489c45ed6639e5049
julianiff <julian.iff@frontify.com>
2024-12-05 16:21:01 +0100

    chore: add context instead of single contexts

--- packages/app-bridge-app/src/react/appContext.ts
+++ packages/app-bridge-app/src/react/appContext.ts
@@ -1,11 +1,11 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
-import { AppBridgePlatformApp, type AssetActionContext, type AssetCreationContext } from '../AppBridgePlatformApp.ts';
+import { AppBridgePlatformApp, type PlatformAppContext } from '../AppBridgePlatformApp.ts';
 
 /**
  * This function just helps to get the Context
  */
-export const appContext = <T extends AssetActionContext | AssetCreationContext>(): T => {
+export const appContext = <T extends PlatformAppContext>(): T => {
     const appBridge = new AppBridgePlatformApp();
     return appBridge.context().get() as T;
 };
