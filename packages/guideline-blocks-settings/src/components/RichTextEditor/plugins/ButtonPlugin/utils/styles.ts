385a911e097e8993cc7c476c43c105e17dfb48be
Peter Tudosa <razvan-petru.tudosa@frontify.com>
2025-01-23 13:09:38 +0100

    feat(RTE): button text overflow
    Signed-off-by: Peter Tudosa <razvan-petru.tudosa@frontify.com>

--- packages/guideline-blocks-settings/src/components/RichTextEditor/plugins/ButtonPlugin/utils/styles.ts
+++ packages/guideline-blocks-settings/src/components/RichTextEditor/plugins/ButtonPlugin/utils/styles.ts
@@ -2,8 +2,20 @@
 
 import { type CSSProperties } from 'react';
 
+const DefaulButtonStyles: CSSProperties = {
+    marginTop: '10px',
+    marginBottom: '10px',
+    display: 'inline-block',
+    wordBreak: 'break-word',
+    overflow: 'hidden',
+    textOverflow: 'ellipsis',
+    maxWidth: '100%',
+    whiteSpace: 'nowrap',
+};
+
 export const BlockButtonStyles: Record<string, CSSProperties & { hover?: CSSProperties }> = {
     buttonPrimary: {
+        ...DefaulButtonStyles,
         fontFamily: 'var(--f-theme-settings-button-primary-font-family)',
         fontSize: 'var(--f-theme-settings-button-primary-font-size)',
         fontWeight: 'var(--f-theme-settings-button-primary-font-weight)',
@@ -19,9 +31,7 @@ export const BlockButtonStyles: Record<string, CSSProperties & { hover?: CSSProp
         borderRadius: 'var(--f-theme-settings-button-primary-border-radius)',
         borderWidth: 'var(--f-theme-settings-button-primary-border-width)',
         color: 'var(--f-theme-settings-button-primary-color)',
-        marginTop: '10px',
-        marginBottom: '10px',
-        display: 'inline-block',
+
         hover: {
             backgroundColor: 'var(--f-theme-settings-button-primary-background-color-hover)',
             borderColor: 'var(--f-theme-settings-button-primary-border-color-hover)',
@@ -30,6 +40,7 @@ export const BlockButtonStyles: Record<string, CSSProperties & { hover?: CSSProp
     },
 
     buttonSecondary: {
+        ...DefaulButtonStyles,
         fontFamily: 'var(--f-theme-settings-button-secondary-font-family)',
         fontSize: 'var(--f-theme-settings-button-secondary-font-size)',
         fontWeight: 'var(--f-theme-settings-button-secondary-font-weight)',
@@ -45,9 +56,6 @@ export const BlockButtonStyles: Record<string, CSSProperties & { hover?: CSSProp
         borderRadius: 'var(--f-theme-settings-button-secondary-border-radius)',
         borderWidth: 'var(--f-theme-settings-button-secondary-border-width)',
         color: 'var(--f-theme-settings-button-secondary-color)',
-        display: 'inline-block',
-        marginTop: '10px',
-        marginBottom: '10px',
         hover: {
             backgroundColor: 'var(--f-theme-settings-button-secondary-background-color-hover)',
             borderColor: 'var(--f-theme-settings-button-secondary-border-color-hover)',
@@ -56,6 +64,7 @@ export const BlockButtonStyles: Record<string, CSSProperties & { hover?: CSSProp
     },
 
     buttonTertiary: {
+        ...DefaulButtonStyles,
         fontFamily: 'var(--f-theme-settings-button-tertiary-font-family)',
         fontSize: 'var(--f-theme-settings-button-tertiary-font-size)',
         fontWeight: 'var(--f-theme-settings-button-tertiary-font-weight)',
@@ -71,9 +80,6 @@ export const BlockButtonStyles: Record<string, CSSProperties & { hover?: CSSProp
         borderRadius: 'var(--f-theme-settings-button-tertiary-border-radius)',
         borderWidth: 'var(--f-theme-settings-button-tertiary-border-width)',
         color: 'var(--f-theme-settings-button-tertiary-color)',
-        display: 'inline-block',
-        marginTop: '10px',
-        marginBottom: '10px',
         hover: {
             backgroundColor: 'var(--f-theme-settings-button-tertiary-background-color-hover)',
             borderColor: 'var(--f-theme-settings-button-tertiary-border-color-hover)',
