7cb1a9aa1736e8d15cacc42a5146ae88ef04d0d0
ragi96 <rafael.giezendanner@frontify.com>
2025-05-01 15:12:52 +0200

    refactor: remove AppBridgeCreateAsset from v4

--- packages/app-bridge/src/errors/NotifyError.ts
+++
@@ -1,8 +0,0 @@
-/* (c) Copyright Frontify Ltd., all rights reserved. */
-
-export class FetchError extends Error {
-    constructor(topic: string) {
-        super(`Call with topic ${topic} failed.`);
-        this.name = 'FetchError';
-    }
-}
