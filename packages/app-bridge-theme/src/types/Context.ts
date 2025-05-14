e8f80db9ebff09c575acc932f22ce65886c4d7ee
Rafael Giezendanner <rafael.giezendanner@frontify.com>
2025-05-14 10:55:46 +0200

    feat: add isPlatformAppsDialogOpen to context (#1218)

--- packages/app-bridge-theme/src/types/Context.ts
+++ packages/app-bridge-theme/src/types/Context.ts
@@ -36,6 +36,7 @@ export type Context = {
     isAuthenticated: boolean;
     isAiBrandAssistantDialogOpen: boolean;
     isSearchDialogOpen: boolean;
+    isPlatformAppsDialogOpen: boolean;
     activeSectionHeadingId: number | null;
     languages: Language[];
     template: TemplateContext | null;
