87b99395546151e6bdb85c960e4b4c14dd99ac16
Anxo Botana <angel.botana@frontify.com>
2025-06-23 12:17:51 +0200

    restore verb

--- packages/app-bridge/src/registries/verbs/Verbs.ts
+++ packages/app-bridge/src/registries/verbs/Verbs.ts
@@ -1,5 +1,5 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
 export type ApiVerb = 'get' | 'create' | 'update' | 'delete' | 'add' | 'remove' | 'set';
-export type CommandVerb = 'open' | 'close' | 'download';
+export type CommandVerb = 'open' | 'close' | 'navigate' | 'download';
 export type EventVerb = 'chosen';
