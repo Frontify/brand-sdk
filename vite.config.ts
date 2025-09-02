5c29df0e44064ca3f0f9ba75e7cc411d1ccfadbc
Daniel Fulop <daniel.fulop@frontify.com>
2025-09-02 17:48:09 +0200

    chore: bump node to v22 (#1278)
    * chore: bump node to v22
    
    * CI test. To revert
    
    * change cli nvm in ci
    
    * bump vite
    
    * Revert "CI test. To revert"
    
    This reverts commit a847a635aacc38016badf31663495424695e200a.

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
     plugins: [react()],
     resolve: {
-        mainFields: ["module", "main"],
+        mainFields: ["browser", "module", "main"],
     },
     server: {
         fs: {
