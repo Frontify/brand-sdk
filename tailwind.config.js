ffe359e228e48649e3bf8d0d61b7f2681ce024c1
fulopdaniel <daniel.fulop@frontify.com>
2025-09-19 13:58:02 +0200

    fix test

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,4 @@
 module.exports = {
-    presets: [require("@frontify/fondue/legacyTokens/tailwind")],
     content: ["./{packages,examples}/*/src/**/*.{ts,tsx}"],
     prefix: "tw-",
     corePlugins: {
