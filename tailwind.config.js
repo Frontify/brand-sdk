060fd57b09ede4f124847f20444d5ad64d67c5bf
ragi96 <rafael.giezendanner@frontify.com>
2025-09-19 12:34:11 +0200

    fix tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 module.exports = {
-    presets: [require("@frontify/fondue/tailwind")],
+    presets: [require("@frontify/fondue/legacyTokens/tailwind")],
     content: ["./{packages,examples}/*/src/**/*.{ts,tsx}"],
+    prefix: "tw-",
     corePlugins: {
         preflight: false,
     },
