f3e72cb60f5ac5a2ff7c2f4b7d4a850b4a116633
ragi96 <rafael.giezendanner@frontify.com>
2025-09-26 13:27:31 +0200

    chore(AppBridge): deprecate page methods

--- packages/app-bridge/src/AppBridgeBlock.ts
+++ packages/app-bridge/src/AppBridgeBlock.ts
@@ -162,25 +162,60 @@ export interface AppBridgeBlock<
 
     getColorsByColorPaletteId(colorPaletteId: number): Promise<Color[]>;
 
+    getAsset(assetId: number): Promise<Asset>;
+
+    /*
+     * @deprecated this method will be removed without a replacement, if you need this please reach out through the frontify-friends channel, happy to discuss your use case!
+     */
     getAllDocuments(): Promise<Document[]>;
 
+    /*
+     * @deprecated this method will be removed without a replacement, if you need this please reach out through the frontify-friends channel, happy to discuss your use case!
+     */
     getUngroupedDocuments(): Promise<Document[]>;
 
+    /*
+     * @deprecated this method will be removed without a replacement, if you need this please reach out through the frontify-friends channel, happy to discuss your use case!
+     */
     getDocumentsByDocumentGroupId(documentGroupId: number): Promise<Document[]>;
 
+    /*
+     * @deprecated this method will be removed without a replacement, if you need this please reach out through the frontify-friends channel, happy to discuss your use case!
+     */
     getDocumentGroups(): Promise<DocumentGroup[]>;
 
+    /*
+     * @deprecated this method will be removed without a replacement, if you need this please reach out through the frontify-friends channel, happy to discuss your use case!
+     */
     getDocumentPagesByDocumentId(documentId: number): Promise<DocumentPage[]>;
 
+    /*
+     * @deprecated this method will be removed without a replacement, if you need this please reach out through the frontify-friends channel, happy to discuss your use case!
+     */
     getDocumentPagesByDocumentCategoryId(documentCategoryId: number): Promise<DocumentPage[]>;
 
+    /*
+     * @deprecated this method will be removed without a replacement, if you need this please reach out through the frontify-friends channel, happy to discuss your use case!
+     */
     getDocumentCategoriesByDocumentId(documentId: number): Promise<DocumentCategory[]>;
 
+    /*
+     * @deprecated this method will be removed without a replacement, if you need this please reach out through the frontify-friends channel, happy to discuss your use case!
+     */
     getUncategorizedDocumentPagesByDocumentId(documentId: number): Promise<DocumentPage[]>;
 
+    /*
+     * @deprecated this method will be removed without a replacement, if you need this please reach out through the frontify-friends channel, happy to discuss your use case!
+     */
     getDocumentSectionsByDocumentPageId(documentPageId: number): Promise<DocumentSection[]>;
 
+    /*
+     * @deprecated this method will be removed without a replacement, if you need this please reach out through the frontify-friends channel, happy to discuss your use case!
+     */
     getDocumentTargets(documentId: number): Promise<DocumentTargets>;
 
+    /*
+     * @deprecated this method will be removed without a replacement, if you need this please reach out through the frontify-friends channel, happy to discuss your use case!
+     */
     getDocumentPageTargets(documentPageId: number): Promise<DocumentPageTargets>;
 }
