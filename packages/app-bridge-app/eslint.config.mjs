51bfe25cd72dbdb81ed74f2edd01b7e841bac346
Julian Iff <julian.iff@frontify.com>
2026-01-06 16:35:51 +0100

    feat: add asssetChooser Context (#1390)
    * chore: add asssetChooser Context
    
    * Create soft-beans-rescue.md
    
    * chore: fix lint
    
    * chore: add build step
    
    * Apply suggestions from code review

--- packages/app-bridge-app/eslint.config.mjs
+++ packages/app-bridge-app/eslint.config.mjs
@@ -19,7 +19,7 @@ export default defineConfig(
         languageOptions: {
             parserOptions: {
                 projectService: true,
-                // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+
                 tsconfigRootDir: import.meta.dirname,
             },
         },
