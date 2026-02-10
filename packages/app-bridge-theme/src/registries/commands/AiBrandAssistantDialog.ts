003a03d17b727a0316e92b2e51675593a7ded692
Mike Trummer <mike.trummer@frontify.com>
2026-02-10 14:38:23 +0100

    chore: deprecate commands

--- packages/app-bridge-theme/src/registries/commands/AiBrandAssistantDialog.ts
+++ packages/app-bridge-theme/src/registries/commands/AiBrandAssistantDialog.ts
@@ -8,6 +8,10 @@ export const openAiBrandAssistantDialog = (): DispatchHandlerParameter<
     CommandRegistry
 > => ({ name: 'openAiBrandAssistantDialog' });
 
+/**
+ * @deprecated This will be removed in version 2.0.0 of `@frontify/app-bridge-theme`
+ * There won't be a replacement for this command.
+ */
 export const closeAiBrandAssistantDialog = (): DispatchHandlerParameter<
     'closeAiBrandAssistantDialog',
     CommandRegistry
