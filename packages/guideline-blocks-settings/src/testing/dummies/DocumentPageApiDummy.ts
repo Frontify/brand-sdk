2a1a1ca9a28a38cecdd087bdcf2c5f195051c863
ragi96 <rafael.giezendanner@frontify.com>
2026-03-16 17:28:23 +0100

    extra line and eslint bump

--- packages/guideline-blocks-settings/src/testing/dummies/DocumentPageApiDummy.ts
+++ packages/guideline-blocks-settings/src/testing/dummies/DocumentPageApiDummy.ts
@@ -1,7 +1,5 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
-/* (c) Copyright Frontify Ltd., all rights reserved. */
-
 enum LinkType {
     External = 'EXTERNAL',
     Internal = 'INTERNAL',
