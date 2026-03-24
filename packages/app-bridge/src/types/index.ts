be1983fead5ff4b212af8765acb0cb5d786b5b13
ragi96 <rafael.giezendanner@frontify.com>
2026-03-24 11:56:08 +0100

    refactor: remove  type and

--- packages/app-bridge/src/types/index.ts
+++ packages/app-bridge/src/types/index.ts
@@ -6,7 +6,6 @@ export * from './Color';
 export * from './ColorPalette';
 export * from './DispatchHandler';
 export * from './Document';
-export * from './DocumentGroup';
 export * from './DocumentPage';
 export * from './DocumentSection';
 export * from './Emitter';
