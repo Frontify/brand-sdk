b92bf0061288e761458cba2ff186c70b218decc4
Peter Tudosa <razvan-petru.tudosa@frontify.com>
2026-03-25 14:45:00 +0100

    fix: missing dummy util
    Signed-off-by: Peter Tudosa <razvan-petru.tudosa@frontify.com>

--- packages/guideline-blocks-settings/src/testing/dummies/DocumentPageApiDummy.ts
+++ packages/guideline-blocks-settings/src/testing/dummies/DocumentPageApiDummy.ts
@@ -1,5 +1,7 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
+import { DocumentPageVisibility, type DocumentPageApi } from '@frontify/app-bridge';
+
 enum LinkType {
     External = 'EXTERNAL',
     Internal = 'INTERNAL',
@@ -113,5 +115,45 @@ export class DocumentApiDummy {
             number_of_document_page_categories: 0,
             number_of_uncategorized_document_pages: 0,
         };
+    }
+}
+
+export class DocumentPageApiDummy {
+    static with(id: number): DocumentPageApi {
+        return {
+            id,
+            creator: 9,
+            created: '2022-08-15T11:47:57.000+00:00',
+            modifier: null,
+            modified: null,
+            document_id: 145,
+            category_id: 29,
+            parent_id: null,
+            valid_from: '2022-08-15T11:47:57.000+00:00',
+            valid_to: null,
+            title: `Document Page Dummy ${id}`,
+            slug: `document-page-dummy-${id}`,
+            sort: 1,
+            link_type: 'INTERNAL',
+            link_url: null,
+            view_count: 0,
+            visibility: DocumentPageVisibility.Everyone,
+            targets: null,
+            permanent_link: `/r/document-page-${id}`,
+            change_processed: false,
+            change_processed_by: null,
+            change_skipped: false,
+            change_skipped_by: null,
+            change_comment: null,
+            change_comment_by: null,
+            change_title: null,
+        };
+    }
+
+    static withFields(fields: Partial<DocumentPageApi> & { id: number }): DocumentPageApi {
+        return {
+            ...DocumentPageApiDummy.with(fields.id),
+            ...fields,
+        } as DocumentPageApi;
     }
 }
