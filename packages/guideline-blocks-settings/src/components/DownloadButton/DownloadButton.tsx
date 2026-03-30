a4b0845bdced8fc2c87e9a5888d18fd06d2ebcdd
fulopdaniel <daniel.fulop@frontify.com>
2026-03-30 10:51:39 +0200

    feat(Buttons): remove custom styles and use fondue

--- packages/guideline-blocks-settings/src/components/DownloadButton/DownloadButton.tsx
+++ packages/guideline-blocks-settings/src/components/DownloadButton/DownloadButton.tsx
@@ -1,37 +1,28 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
-import { Tooltip } from '@frontify/fondue/components';
+import { Button, Tooltip } from '@frontify/fondue/components';
 import { IconArrowCircleDown } from '@frontify/fondue/icons';
-import { useFocusRing } from '@react-aria/focus';
-
-import { joinClassNames } from '../../utilities';
 
 import { type DownloadButtonProps } from './types';
 
 export const DownloadButton = ({ onDownload, ariaLabel }: DownloadButtonProps) => {
-    const { isFocused, focusProps } = useFocusRing();
-
     return (
         <Tooltip.Root enterDelay={500}>
             <Tooltip.Trigger asChild>
-                <button
-                    type="button"
-                    tabIndex={0}
+                <Button
                     aria-label={ariaLabel ?? 'Download'}
-                    {...focusProps}
-                    className={joinClassNames([
-                        'tw-outline-none tw-rounded-medium',
-                        isFocused &&
-                            'tw-ring-4 tw-ring-blue tw-ring-offset-2 dark:tw-ring-offset-black tw-outline-none',
-                    ])}
-                    onClick={onDownload}
-                    onPointerDown={(e) => e.preventDefault()}
+                    onPress={onDownload}
                     data-test-id="download-button"
+                    aspect="square"
+                    emphasis="default"
+                    rounding="full"
+                    size="small"
+                    type="button"
+                    variant="default"
+                    hugWidth
                 >
-                    <span className="tw-flex tw-text-x-small tw-font-primary tw-items-center tw-gap-1 tw-rounded-full tw-bg-primary-on-primary hover:tw-bg-primary-on-primary active:tw-bg-primary-on-primary tw-text-primary tw-outline tw-outline-1 tw-outline-offset-1 tw-p-1.5 tw-outline-line-mid">
-                        <IconArrowCircleDown size="16" />
-                    </span>
-                </button>
+                    <IconArrowCircleDown size={16} />
+                </Button>
             </Tooltip.Trigger>
             <Tooltip.Content side="top">Download</Tooltip.Content>
         </Tooltip.Root>
