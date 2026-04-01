08c7fa989adbd3ce511b7083db8b19f4a89383b2
ragi96 <rafael.giezendanner@frontify.com>
2026-04-01 17:14:56 +0200

    dummies

--- packages/app-bridge/src/tests/AssetApiDummy.ts
+++ packages/app-bridge/src/tests/AssetApiDummy.ts
@@ -6,7 +6,6 @@ export class AssetApiDummy {
     static with(id: number): AssetApi {
         return {
             id,
-            creator_name: 'Creator Name',
             alternative_text: 'Alternative Text',
             ext: 'png',
             external_url: null,
@@ -18,8 +17,6 @@ export class AssetApiDummy {
             file_origin_url: 'https://origin.url',
             preview_url: 'https://preview.url',
             project_id: 23,
-            project_name: null,
-            project_type: 'STYLEGUIDE',
             file_size: 256,
             file_size_formatted: '123.45 MB',
             status: 'FINISHED',
