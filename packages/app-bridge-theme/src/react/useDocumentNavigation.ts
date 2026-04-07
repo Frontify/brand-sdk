755888778539b7403da8204f83e04fdbe023360b
Anxo Botana <angel.botana@frontify.com>
2026-04-07 12:35:36 +0200

    feat(AppBridgeTheme): ensure we hydrateDocumentNavigation when PortalNavigationItems are loaded

--- packages/app-bridge-theme/src/react/useDocumentNavigation.ts
+++ packages/app-bridge-theme/src/react/useDocumentNavigation.ts
@@ -4,17 +4,34 @@ import { useEffect, useSyncExternalStore } from 'react';
 
 import { type AppBridgeTheme } from '../AppBridgeTheme.ts';
 import { hydrateContextDocumentNavigation } from '../registries/commands/HydrateContextDocumentNavigation';
-import { type Document, type DocumentNavigationItem, type DocumentChildNavigationItem } from '../types/Guideline';
+import {
+    type Document,
+    type DocumentNavigationItem,
+    type DocumentChildNavigationItem,
+    type PortalNavigationItem,
+} from '../types/Guideline';
 
 export const useDocumentNavigation = (appBridge: AppBridgeTheme, document: DocumentNavigationItem | Document) => {
+    const portalNavigation: Readonly<PortalNavigationItem[] | null> = useSyncExternalStore(
+        appBridge.context('portalNavigation').subscribe,
+        appBridge.context('portalNavigation').get,
+    );
+
     const documentNavigation: Record<number, DocumentChildNavigationItem[] | undefined> = useSyncExternalStore(
         appBridge.context('documentNavigation').subscribe,
         appBridge.context('documentNavigation').get,
     );
 
     useEffect(() => {
+        console.log(
+            'useDocumentNavigation - document or portalNavigation changed, dispatching hydrateContextDocumentNavigation',
+            {
+                document,
+                portalNavigation,
+            },
+        );
         appBridge.dispatch(hydrateContextDocumentNavigation(document.id()));
-    }, [appBridge, document]);
+    }, [appBridge, document, portalNavigation]);
 
     return documentNavigation ? (documentNavigation[document.id()] ?? []) : [];
 };
