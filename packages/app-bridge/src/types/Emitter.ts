954805758cee24fe5b23185ef413a1ad8cfb4100
ragi96 <rafael.giezendanner@frontify.com>
2026-04-16 15:37:00 +0200

    fix

--- packages/app-bridge/src/types/Emitter.ts
+++ packages/app-bridge/src/types/Emitter.ts
@@ -2,6 +2,7 @@
 
 import { type BlockSettingsUpdateEvent } from '../react/useBlockSettings';
 
+import { type Asset } from './Asset';
 import { type PrivacySettings } from './PrivacySettings';
 import { type Template } from './Template';
 import { type AssetViewerOptions } from './Terrific';
@@ -13,6 +14,12 @@ export type EmitterEvents = {
         blockId: number;
         blockTemplates: Record<string, Template[]>;
         prevBlockTemplates: Record<string, Template[]>;
+    };
+
+    'AppBridge:BlockAssetsUpdated': {
+        blockId: number;
+        blockAssets: Record<string, Asset[]>;
+        prevBlockAssets: Record<string, Asset[]>;
     };
 
     'AppBridge:PrivacySettingsChanged': PrivacySettings;
