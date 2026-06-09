ac7388fc5551a63ca47f8cc578e6cb83cf355196
Ilham Moutaharik <ilham.moutaharik@frontify.com>
2026-06-09 16:29:43 +0200

    fix(CLI): Fondue setup in the CLI platform-app templates

--- packages/cli/templates/platform-app-tailwind/tailwind.config.ts
+++ packages/cli/templates/platform-app-tailwind/tailwind.config.ts
@@ -1,6 +1,8 @@
+import frontifyTailwindConfig from '@frontify/fondue/tokens/tailwind';
 import { type Config } from 'tailwindcss';
 
 export default {
+    presets: [frontifyTailwindConfig],
     content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
     theme: {
         extend: {},
