23729fdecd15adbeff5d5c75b1460ded72beeba4
ragi96 <rafael.giezendanner@frontify.com>
2026-07-09 11:28:40 +0200

    fix

--- packages/app-bridge/src/registries/commands/PlatformApp.ts
+++ packages/app-bridge/src/registries/commands/PlatformApp.ts
@@ -5,7 +5,7 @@ import { type DispatchHandlerParameter } from '../../AppBridge';
 import { type CommandRegistry } from './CommandRegistry';
 
 export const openPlatformAppDirect = (
-    payload: CommandRegistry['openPlatformAppDirect']
+    payload: CommandRegistry['openPlatformAppDirect'],
 ): DispatchHandlerParameter<'openPlatformAppDirect', CommandRegistry> => ({
     name: 'openPlatformAppDirect',
     payload,
